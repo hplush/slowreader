646033af556907f5fea907f0a270556f81980272
Andrey Sitnik <andrey@sitnik.ru>
2021-11-04 00:03:01 +0100

    Fix Vite bug

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
 
 export default defineConfig(() => ({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    resolve({
+      extensions: ['.js', '.ts']
+    })
+  ]
 }))
