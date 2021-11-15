1d1a36646b74bf36fc14babf3db78f6576053597
Andrey Sitnik <andrey@sitnik.ru>
2021-11-15 19:53:30 +0100

    Fix Storybook preview

--- web/.storybook/preview.cjs
+++ web/.storybook/preview.cjs
@@ -1,9 +1,13 @@
+import '../main/reset.css'
+import '../main/colors.css'
+import '../main/index.css'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 }
