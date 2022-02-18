00d60412f84f634f96e5e3e7104853ee7b75123c
Andrey Sitnik <andrey@sitnik.ru>
2022-02-18 13:10:35 +0100

    Add note for eslint-disable

--- core/preview/index.ts
+++ core/preview/index.ts
@@ -2,6 +2,7 @@ import { createResource, isValidResource } from '../resource/index.js'
 import { findSource, SourceName } from '../sources/index.js'
 
 export function getSourceFromUrl(dirtyUrl: string): SourceName | 'unknown' {
+  // TS guards doesn’t work with `let`
   // eslint-disable-next-line prefer-let/prefer-let
   const resource = createResource(dirtyUrl)
   if (isValidResource(resource)) {
