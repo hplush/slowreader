164e7caa47a73e80d193d7396bfb87d01c53ca71
Andrey Sitnik <andrey@sitnik.ru>
2023-07-01 22:49:12 +0200

    Clean up file names

--- scripts/check-versions.js
+++ scripts/check-versions.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
@@ -9,13 +8,12 @@ const ROOT = join(fileURLToPath(import.meta.url), '..', '..')
 function read(...parts) {
   return readFileSync(join(ROOT, ...parts)).toString()
 }
-
 let pkg = read('package.json')
-let versions = read('.tool-versions')
+let toolVersions = read('.tool-versions')
 
-let minor = versions.match(/nodejs (\d+.\d+)\./)[1]
+let nodeMinor = toolVersions.match(/nodejs (\d+\.\d+)\./)[1]
 
-if (!pkg.includes(`"node": "~${minor}.`)) {
+if (!pkg.includes(`"node": "~${nodeMinor}.`)) {
   process.stderr.write(
     '.tool-versions and package.json have different Node.js minor version\n'
   )
