b824a7373b49135a50ae977599e43c031614b8dd
Andrey Sitnik <andrey@sitnik.ru>
2023-09-07 17:39:21 +0200

    Move PostCSS config to JSON format

--- web/postcss.config.js
+++
@@ -1,7 +0,0 @@
-import oklabFunction from '@csstools/postcss-oklab-function'
-import autoprefixer from 'autoprefixer'
-import darkThemeClass from 'postcss-dark-theme-class'
-
-export default {
-  plugins: [oklabFunction, autoprefixer, darkThemeClass]
-}
