810fa0fd29014715be08a6a0b39394b366ba7c6e
Andrey Sitnik <andrey@sitnik.ru>
2023-09-10 00:12:11 +0200

    Add UiRadio

--- web/pages/settings.svelte
+++ web/pages/settings.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
   import { settingsMessages as t, theme } from '@slowreader/core'
+
+  import UiRadio from '../ui/radio.svelte'
 </script>
 
-<fieldset>
-  <legend>{$t.theme}</legend>
-  <label>
-    <input type="radio" value="system" bind:group={$theme} />
-    {$t.system}
-  </label>
-  <label>
-    <input type="radio" value="light" bind:group={$theme} />
-    {$t.light}
-  </label>
-  <label>
-    <input type="radio" value="dark" bind:group={$theme} />
-    {$t.dark}
-  </label>
-</fieldset>
+<UiRadio
+  store={theme}
+  title={$t.theme}
+  values={[
+    ['system', $t.system],
+    ['light', $t.light],
+    ['dark', $t.dark]
+  ]}
+/>
---
+++ web/ui/radio.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { ReadableAtom } from 'nanostores'
+
+  export let title: string
+  export let values: [string, string][]
+  export let store: ReadableAtom<string>
+</script>
+
+<fieldset>
+  <legend>{title}</legend>
+  {#each values as [value, name] (value)}
+    <label>
+      <input type="radio" {value} bind:group={$store} />
+      {name}
+    </label>
+  {/each}
+</fieldset>
