17ee1e39193ed9f3aab9b63e7f74c27a59b2573d
Andrey Sitnik <andrey@sitnik.ru>
2023-10-07 17:50:04 +0200

    Update dependencies

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
+// Until we have type conflict between Vite and @rollup/plugin-node-resolve
+// because of the different dependencies versions
+// @ts-expect-error
 export default defineConfig(() => ({
   plugins: [
     svelte(),
