52367303cf4c5148c0fcad7d5e505f8009a720e2
Andrey Sitnik <andrey@sitnik.ru>
2023-11-21 08:31:02 +0100

    Clean up code

--- web/main/index.ts
+++ web/main/index.ts
@@ -20,18 +20,18 @@ router.subscribe(page => {
   }
 })
 
+let root = document.documentElement
+
 router.subscribe(route => {
-  document.documentElement.classList.toggle('is-slow', isSlowRoute(route))
-  document.documentElement.classList.toggle('is-guest', isGuestRoute(route))
+  root.classList.toggle('is-slow', isSlowRoute(route))
+  root.classList.toggle('is-guest', isGuestRoute(route))
 })
 
 theme.subscribe(themeValue => {
-  document.documentElement.classList.toggle('is-dark', themeValue === 'dark')
-  document.documentElement.classList.toggle('is-light', themeValue === 'light')
+  root.classList.toggle('is-dark', themeValue === 'dark')
+  root.classList.toggle('is-light', themeValue === 'light')
 })
 
-const main = new Main({
-  target: document.getElementById('main')!
-})
+const main = new Main({ target: document.getElementById('main')! })
 
 export default main
