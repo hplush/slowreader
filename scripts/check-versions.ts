adcb5dddce20707493be6c7ae5c989e66f9e089e
Andrey Sitnik <andrey@sitnik.ru>
2023-11-23 07:50:29 +0100

    Clean up dependency versions

--- scripts/check-versions.ts
+++ scripts/check-versions.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'node:fs'
+import { existsSync, readdirSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import pico from 'picocolors'
@@ -13,11 +13,26 @@ let toolVersions = read('.tool-versions')
 
 let nodeMinor = toolVersions.match(/nodejs (\d+\.\d+)\./)![1]
 
-if (!pkg.includes(`"node": "~${nodeMinor}.`)) {
-  process.stderr.write(
-    pico.red(
-      '.tool-versions and package.json have different Node.js minor version\n'
-    )
-  )
+function error(msg: string): void {
+  process.stderr.write(pico.red(`${msg}\n`))
   process.exit(1)
+}
+
+if (!pkg.includes(`"node": "~${nodeMinor}.`)) {
+  error('.tool-versions and package.json have different Node.js minor version')
+}
+
+function checkDependencies(file: string, content: string): void {
+  let match = content.match(/"[^"]+": "[\^~][^"]+"/)
+  if (match && !match[0].startsWith('"node":')) {
+    error(`Not locked version in ${file}: ${pico.yellow(match[0])}`)
+  }
+}
+
+checkDependencies('package.json', pkg)
+for (let child of readdirSync(ROOT)) {
+  if (existsSync(join(ROOT, child, 'package.json'))) {
+    let relative = join(child, 'package.json')
+    checkDependencies(relative, read(relative))
+  }
 }
