f0ae912b8a81a02bd0562b7709f6425707034b8f
Andrey Sitnik <andrey@sitnik.ru>
2023-11-26 17:25:38 +0100

    Add base card style

---
+++ web/stories/ui/card.stories.svelte
@@ -0,0 +1,38 @@
+<script context="module" lang="ts">
+  import UiCard from '../../ui/card.svelte'
+
+  export const meta = {
+    component: UiCard,
+    title: 'UI/Card'
+  }
+</script>
+
+<script lang="ts">
+  import { Story } from '@storybook/addon-svelte-csf'
+
+  import Section from '../section.svelte'
+</script>
+
+<Story name="Base">
+  <Section>
+    <UiCard>Test</UiCard>
+  </Section>
+</Story>
+
+<Story name="Slow" parameters={{ themes: { themeOverride: 'lightSlow' } }}>
+  <Section>
+    <UiCard>Test</UiCard>
+  </Section>
+</Story>
+
+<Story name="Dark" parameters={{ themes: { themeOverride: 'dark' } }}>
+  <Section>
+    <UiCard>Test</UiCard>
+  </Section>
+</Story>
+
+<Story name="Dark Slow" parameters={{ themes: { themeOverride: 'darkSlow' } }}>
+  <Section>
+    <UiCard>Test</UiCard>
+  </Section>
+</Story>
---
+++ web/ui/card.svelte
@@ -0,0 +1,13 @@
+<section class="card">
+  <slot />
+</section>
+
+<style>
+  .card {
+    padding: var(--padding-l);
+    background-color: var(--card-color);
+    border-top: 1px solid var(--top-edge-color);
+    border-bottom: 1px solid var(--bottom-edge-color);
+    border-radius: var(--radius);
+  }
+</style>
