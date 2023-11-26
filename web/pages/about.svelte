c6f8f8221646dd23d5921880c7ac3246da2373ff
Andrey Sitnik <andrey@sitnik.ru>
2023-11-26 17:29:59 +0100

    Use card in app to fix linters

--- web/pages/about.svelte
+++ web/pages/about.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import { aboutMessages as t } from '@slowreader/core/messages'
+
+  import UiCard from '../ui/card.svelte'
 </script>
 
-<a href="https://github.com/hplush/slowreader" target="_blank">{$t.source}</a>
+<UiCard>
+  <a href="https://github.com/hplush/slowreader" target="_blank">{$t.source}</a>
+</UiCard>
