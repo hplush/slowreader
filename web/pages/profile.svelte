ac9cae2e872ef7208c9800f27ef7e84d7d31c5f6
Andrey Sitnik <andrey@sitnik.ru>
2023-11-27 11:04:27 +0100

    Use button on page to fix linter

--- web/pages/profile.svelte
+++ web/pages/profile.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import { signOut } from '@slowreader/core'
   import { profileMessages as t } from '@slowreader/core/messages'
+
+  import UiButton from '../ui/button.svelte'
 </script>
 
-<button
+<UiButton
   on:click={() => {
     signOut()
-  }}>{$t.delete}</button
+  }}
 >
+  {$t.delete}
+</UiButton>
