fc508f740c9993b6fdc884a2c9548053ba285fbb
Andrey Sitnik <andrey@sitnik.ru>
2023-11-28 17:24:14 +0100

    Clean up start script

---
+++ web/main/browser.ts
@@ -0,0 +1,33 @@
+import { getPagePath } from '@nanostores/router'
+import { isGuestRoute, isSlowRoute, router, theme } from '@slowreader/core'
+
+import { locale } from '../stores/locale.js'
+import { urlRouter } from '../stores/router.js'
+
+router.subscribe(page => {
+  if (page.redirect) {
+    let href
+    if (page.route === 'preview') {
+      href = getPagePath(urlRouter, page.route, page.params)
+    } else {
+      href = getPagePath(urlRouter, page.route)
+    }
+    urlRouter.open(href)
+  }
+})
+
+let root = document.documentElement
+
+router.subscribe(route => {
+  root.classList.toggle('is-slow', isSlowRoute(route))
+  root.classList.toggle('is-guest', isGuestRoute(route))
+})
+
+theme.subscribe(themeValue => {
+  root.classList.toggle('is-dark', themeValue === 'dark')
+  root.classList.toggle('is-light', themeValue === 'light')
+})
+
+locale.subscribe(localeValue => {
+  root.lang = localeValue
+})
--- web/main/index.ts
+++ web/main/index.ts
@@ -1,43 +1,11 @@
 import './environment.js'
+import './browser.js'
 
 import './index.css'
 
-import { getPagePath } from '@nanostores/router'
-import { isGuestRoute, isSlowRoute, router, theme } from '@slowreader/core'
-
-import { locale } from '../stores/locale.js'
-import { urlRouter } from '../stores/router.js'
 import Main from './main.svelte'
 
-router.subscribe(page => {
-  if (page.redirect) {
-    let href
-    if (page.route === 'preview') {
-      href = getPagePath(urlRouter, page.route, page.params)
-    } else {
-      href = getPagePath(urlRouter, page.route)
-    }
-    urlRouter.open(href)
-  }
-})
-
-let root = document.documentElement
-
-router.subscribe(route => {
-  root.classList.toggle('is-slow', isSlowRoute(route))
-  root.classList.toggle('is-guest', isGuestRoute(route))
-})
-
-theme.subscribe(themeValue => {
-  root.classList.toggle('is-dark', themeValue === 'dark')
-  root.classList.toggle('is-light', themeValue === 'light')
-})
-
-locale.subscribe(localeValue => {
-  root.lang = localeValue
-})
-
 document.getElementById('loader')?.remove()
-const main = new Main({ target: document.getElementById('main')! })
 
-export default main
+let target = document.getElementById('main')
+if (target) new Main({ target })
