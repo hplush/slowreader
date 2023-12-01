b33913c7ef355171c1a3c94ba1f76fe97a1a55f4
Andrey Sitnik <andrey@sitnik.ru>
2023-12-01 15:21:14 +0100

    Fix button hotkey position

--- web/ui/hotkey.svelte
+++ web/ui/hotkey.svelte
@@ -2,14 +2,20 @@
   export let hotkey: string
 </script>
 
-<kbd class="hotkey">{hotkey}</kbd>
+<kbd class="hotkey">
+  <div class="hotkey_float">{hotkey}</div>
+</kbd>
 
 <style>
   .hotkey {
-    position: relative;
-    top: -6px;
     display: block;
-    margin-inline: -3px -7px;
+    margin-inline-start: -4px;
+  }
+
+  .hotkey_float {
+    position: absolute;
+    inset-inline-end: var(--padding-m);
+    top: calc(var(--padding-m) - 1px);
     font: var(--hotkey-font);
     color: var(--hotkey-color);
     text-transform: uppercase;
--- web/ui/navbar/switcher.svelte
+++ web/ui/navbar/switcher.svelte
@@ -28,8 +28,8 @@
 
 <div class="navbar-switcher">
   <a
-    bind:this={slow}
     class="navbar-switcher_link"
+    bind:this={slow}
     aria-current={$router.route === 'slowAll' ? 'page' : null}
     href={getURL('slowAll')}
   >
@@ -42,8 +42,8 @@
     </div>
   </a>
   <a
-    bind:this={fast}
     class="navbar-switcher_link"
+    bind:this={fast}
     aria-current={$router.route === 'fast' ? 'page' : null}
     href={getURL('fast')}
   >
