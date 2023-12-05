3114c8387c67c960841fafef49f2bd9fcf567087
Andrey Sitnik <andrey@sitnik.ru>
2023-12-05 17:39:24 +0100

    Add styles for UiRadio

---
+++ web/stories/ui/radio.stories.svelte
@@ -0,0 +1,92 @@
+<script context="module" lang="ts">
+  import UiRadio from '../../ui/radio.svelte'
+
+  export const meta = {
+    component: UiRadio,
+    title: 'UI/Radio'
+  }
+</script>
+
+<script lang="ts">
+  import { Story } from '@storybook/addon-svelte-csf'
+  import { atom } from 'nanostores'
+
+  import UiCard from '../../ui/card.svelte'
+  import UiParagraph from '../../ui/paragraph.svelte'
+  import Section from '../section.svelte'
+
+  let store = atom<string>('1')
+</script>
+
+<Story name="Base">
+  <Section width={500}>
+    <UiCard>
+      <UiRadio
+        {store}
+        title="Normal radio"
+        values={[
+          ['1', 'First option'],
+          ['2', 'Second option'],
+          ['3', 'Third option']
+        ]}
+      />
+      <UiParagraph>After</UiParagraph>
+    </UiCard>
+  </Section>
+  <Section hover="label:first-of-type" width={500}>
+    <UiCard>
+      <UiRadio
+        {store}
+        title="Hover"
+        values={[
+          ['1', 'First option'],
+          ['2', 'Second option']
+        ]}
+      />
+      <UiParagraph>After</UiParagraph>
+    </UiCard>
+  </Section>
+  <Section active="label:first-of-type" width={500}>
+    <UiCard>
+      <UiRadio
+        {store}
+        title="Active"
+        values={[
+          ['1', 'First option'],
+          ['2', 'Second option']
+        ]}
+      />
+      <UiParagraph>After</UiParagraph>
+    </UiCard>
+  </Section>
+  <Section focus="label:first-of-type" width={500}>
+    <UiCard>
+      <UiRadio
+        {store}
+        title="Focus"
+        values={[
+          ['1', 'First option'],
+          ['2', 'Second option']
+        ]}
+      />
+      <UiParagraph>After</UiParagraph>
+    </UiCard>
+  </Section>
+</Story>
+
+<Story name="Dark" parameters={{ themes: { themeOverride: 'dark' } }}>
+  <Section width={500}>
+    <UiCard>
+      <UiRadio
+        {store}
+        title="Normal radio"
+        values={[
+          ['1', 'First option'],
+          ['2', 'Second option'],
+          ['3', 'Third option']
+        ]}
+      />
+      <UiParagraph>After</UiParagraph>
+    </UiCard>
+  </Section>
+</Story>
--- web/ui/radio.svelte
+++ web/ui/radio.svelte
@@ -4,14 +4,144 @@
   export let title: string
   export let store: ReadableAtom<string>
   export let values: [string, string][]
+
+  function nextLabel(label: HTMLLabelElement): HTMLLabelElement {
+    let next = label.nextElementSibling as HTMLLabelElement | undefined
+    if (next) {
+      return next
+    } else {
+      return label.parentElement!.querySelector<HTMLLabelElement>('label')!
+    }
+  }
+
+  function prevLabel(label: HTMLLabelElement): HTMLLabelElement {
+    let prev = label.previousElementSibling as HTMLLabelElement | undefined
+    if (prev) {
+      return prev
+    } else {
+      return label.parentElement!.querySelector<HTMLLabelElement>(
+        'label:last-child'
+      )!
+    }
+  }
+
+  function keyUp(e: KeyboardEvent): void {
+    let label = e.target as HTMLLabelElement
+    if (e.key === 'ArrowUp') {
+      e.preventDefault()
+      let prev = prevLabel(label)
+      prev.click()
+      prev.focus()
+    } else if (e.key === 'ArrowDown') {
+      let next = nextLabel(label)
+      next.click()
+      next.focus()
+    }
+  }
 </script>
 
-<fieldset>
-  <legend>{title}</legend>
+<fieldset class="radio" role="radiogroup">
+  <legend class="radio_label">{title}</legend>
   {#each values as [value, name] (value)}
-    <label>
-      <input type="radio" {value} bind:group={$store} />
+    <!-- svelte-ignore a11y-no-noninteractive-element-to-interactive-role -->
+    <label
+      class="radio_value"
+      aria-checked={$store === value}
+      role="radio"
+      tabindex={$store === value ? 0 : -1}
+      on:keyup={keyUp}
+    >
+      <input
+        class="radio_input"
+        tabindex="-1"
+        type="radio"
+        {value}
+        bind:group={$store}
+      />
+      <div class="radio_fake"></div>
       {name}
     </label>
   {/each}
 </fieldset>
+
+<style>
+  .radio {
+    margin-bottom: calc(
+      var(--outer-radius) - var(--padding-l) + var(--card-text-fix)
+    );
+  }
+
+  .radio_label {
+    padding-bottom: var(--padding-l);
+    font-weight: bold;
+  }
+
+  .radio_value {
+    position: relative;
+    padding-block: var(--padding-l);
+    padding-inline: calc(3 * var(--padding-l)) var(--padding-l);
+    margin-inline: calc(-1 * var(--padding-l));
+    cursor: pointer;
+    border-top: 1px solid var(--zone-color);
+
+    &:last-child {
+      border-bottom: 1px solid var(--zone-color);
+    }
+
+    &:hover {
+      background: var(--hover-color);
+    }
+
+    &:active {
+      padding-block: calc(var(--padding-l) + 1px) calc(var(--padding-l) - 1px);
+      box-shadow:
+        inset -2px 0 0 var(--land-color),
+        inset 2px 0 0 var(--land-color),
+        5px 0 0 var(--land-color),
+        -5px 0 0 var(--land-color),
+        inset 0 1px 2px oklch(from var(--shadow-color) l c h / 20%);
+    }
+  }
+
+  .radio_input {
+    display: none;
+  }
+
+  .radio_fake {
+    position: absolute;
+    inset-inline-start: var(--padding-l);
+    top: calc(50% - var(--padding-l) / 2);
+    box-sizing: border-box;
+    display: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: var(--padding-l);
+    height: var(--padding-l);
+    vertical-align: middle;
+    border: 2px solid var(--zone-color);
+    border-radius: 50%;
+    transition: border 200ms;
+  }
+
+  input:checked + .radio_fake {
+    border-color: var(--text-color);
+  }
+
+  .radio_fake::after {
+    display: block;
+    width: 0;
+    height: 0;
+    content: '';
+    background: var(--text-color);
+    border-radius: 50%;
+    transition:
+      height 200ms cubic-bezier(0.34, 1.56, 0.64, 1),
+      width 200ms cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+
+  input:checked + .radio_fake::after {
+    width: var(--padding-m);
+    height: var(--padding-m);
+  }
+</style>
