7cfe3c3b488b558a07ff989483d660077807fd37
Andrey Sitnik <andrey@sitnik.ru>
2023-12-05 08:53:13 +0100

    Fix card padding for text

--- web/ui/card-actions.svelte
+++ web/ui/card-actions.svelte
@@ -6,6 +6,7 @@
   .card-actions {
     display: flex;
     gap: var(--padding-m);
+    padding-bottom: var(--card-text-fix);
     margin-top: var(--padding-l);
   }
 </style>
--- web/ui/card.svelte
+++ web/ui/card.svelte
@@ -3,8 +3,12 @@
 </section>
 
 <style>
+  :root {
+    --card-text-fix: 3px;
+  }
+
   .card {
-    padding: var(--padding-l);
+    padding: calc(var(--padding-l) - var(--card-text-fix)) var(--padding-l);
     background-color: var(--card-color);
     border-top: 1px solid var(--top-edge-color);
     border-bottom: 1px solid var(--bottom-edge-color);
