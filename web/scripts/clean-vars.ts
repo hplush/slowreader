3d44a502db678b5b8b5ef6aaaa235be2c80e1c63
Andrey Sitnik <andrey@sitnik.ru>
2023-12-07 06:48:17 +0100

    Move PostCSS plugins to separated dir

--- web/scripts/clean-vars.ts
+++ web/scripts/clean-vars.ts
@@ -1,49 +1,9 @@
 import { readFile, writeFile } from 'node:fs/promises'
-import postcss, { type Container, type Node, type Plugin } from 'postcss'
+import postcss from 'postcss'
+
+import cleaner from '../postcss/vars-cleaner.js'
 
 let files = process.argv.slice(2)
-
-const SILENT = /^--[a-z]+-\d\d\d?$/
-
-function removeWithEmptyParent(node: Node): void {
-  let parent = node.parent as Container
-  node.remove()
-  if (parent.nodes.length === 0) {
-    removeWithEmptyParent(parent)
-  }
-}
-
-let cleaner: Plugin = {
-  postcssPlugin: 'clean-vars',
-  prepare() {
-    let used = new Set<string>()
-    let vars = new Map<string, Node[]>()
-    return {
-      Declaration(decl) {
-        if (decl.prop.startsWith('--')) {
-          let nodes = vars.has(decl.prop) ? vars.get(decl.prop)! : []
-          vars.set(decl.prop, nodes.concat(decl))
-          decl.raws.between = ':'
-        }
-        if (decl.value.includes('var(--')) {
-          let name = decl.value.match(/var\((--[^)]+)\)/)![1]!
-          used.add(name)
-        }
-      },
-      OnceExit() {
-        for (let [name, decls] of vars) {
-          if (!used.has(name)) {
-            if (SILENT.test(name)) {
-              for (let decl of decls) removeWithEmptyParent(decl)
-            } else {
-              throw decls[0]!.error(`Unused CSS variable ${name}`)
-            }
-          }
-        }
-      }
-    }
-  }
-}
 
 await Promise.all(
   files.map(async path => {
