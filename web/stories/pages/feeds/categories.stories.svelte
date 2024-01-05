16d9dc6e1516507c0d68b457d5afaadbef30fe48
Andrey Sitnik <andrey@sitnik.ru>
2024-01-05 16:31:32 +0100

    Add feeds by categories stories

---
+++ web/stories/pages/feeds/categories.stories.svelte
@@ -0,0 +1,54 @@
+<script context="module" lang="ts">
+  import FeedsCategories from '../../../pages/feeds/categories.svelte'
+
+  export const meta = {
+    component: FeedsCategories,
+    title: 'Pages/Feeds/Categories'
+  }
+</script>
+
+<script lang="ts">
+  import type { FeedValue } from '@slowreader/core'
+  import { Story } from '@storybook/addon-svelte-csf'
+
+  import Scene from '../../scene.svelte'
+
+  const FEEDS: Partial<FeedValue>[] = [
+    {
+      categoryId: 'code',
+      id: 'a',
+      loader: 'atom',
+      title: 'Local-first',
+      url: 'https://example.com/news.atom'
+    },
+    {
+      categoryId: 'code',
+      title:
+        'Very long title of the feed where we discuss should we use TypeScript or a better typed language'
+    },
+    { categoryId: 'unknown', title: 'Broken category' },
+    { title: 'My friend' }
+  ]
+</script>
+
+<Story name="Empty" parameters={{ layout: 'fullscreen' }}>
+  <Scene feeds={[]}>
+    <FeedsCategories feedId={undefined} />
+  </Scene>
+</Story>
+
+<Story name="List" parameters={{ layout: 'fullscreen' }}>
+  <Scene categories={[{ id: 'code', title: 'Code' }]} feeds={FEEDS}>
+    <FeedsCategories feedId={undefined} />
+  </Scene>
+</Story>
+
+<Story name="Feed Loading" parameters={{ layout: 'fullscreen' }}>
+  <Scene
+    categories={[{ id: 'code', title: 'Code' }]}
+    feeds={FEEDS}
+    responses={{ 'https://example.com/news.atom': { loading: true } }}
+  >
+    <FeedsCategories feedId="a" />
+  </Scene>
+</Story>
