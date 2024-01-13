694585493880b8d7e4f00c32b8193ad94c8901fd
Andrey Sitnik <andrey@sitnik.ru>
2024-01-13 22:30:25 +0100

    Reduce stories number

--- web/stories/ui/card.stories.svelte
+++ web/stories/ui/card.stories.svelte
@@ -13,7 +13,6 @@
   import Button from '../../ui/button.svelte'
   import CardActions from '../../ui/card-actions.svelte'
   import Paragraph from '../../ui/paragraph.svelte'
-  import Scene from '../scene.svelte'
   import Section from '../section.svelte'
 </script>
 
@@ -45,36 +44,6 @@
   </Section>
 </Story>
 
-<Story name="Slow" parameters={{ themes: { themeOverride: 'light' } }}>
-  <Scene slow>
-    <Section width={500}>
-      <Card>
-        <Paragraph>
-          A fireplace or hearth is a structure made of brick, stone or metal
-          designed to contain a fire. Fireplaces are used for the relaxing
-          ambiance they create and for heating a room. Modern fireplaces vary in
-          heat efficiency, depending on the design.
-        </Paragraph>
-        <Paragraph>
-          Fireplaces are used for the relaxing ambiance they create and for
-          heating a room. Modern fireplaces vary in heat efficiency, depending
-          on the design.
-        </Paragraph>
-      </Card>
-    </Section>
-
-    <Section width={500}>
-      <Card>
-        <Paragraph>Are you really want to delete profile?</Paragraph>
-        <CardActions>
-          <Button>Yes</Button>
-          <Button>No</Button>
-        </CardActions>
-      </Card>
-    </Section>
-  </Scene>
-</Story>
-
 <Story name="Dark" parameters={{ themes: { themeOverride: 'dark' } }}>
   <Section width={500}>
     <Card>
@@ -101,34 +70,4 @@
       </CardActions>
     </Card>
   </Section>
-</Story>
-
-<Story name="Dark Slow" parameters={{ themes: { themeOverride: 'dark' } }}>
-  <Scene slow>
-    <Section width={500}>
-      <Card>
-        <Paragraph>
-          A fireplace or hearth is a structure made of brick, stone or metal
-          designed to contain a fire. Fireplaces are used for the relaxing
-          ambiance they create and for heating a room. Modern fireplaces vary in
-          heat efficiency, depending on the design.
-        </Paragraph>
-        <Paragraph>
-          Fireplaces are used for the relaxing ambiance they create and for
-          heating a room. Modern fireplaces vary in heat efficiency, depending
-          on the design.
-        </Paragraph>
-      </Card>
-    </Section>
-
-    <Section width={500}>
-      <Card>
-        <Paragraph>Are you really want to delete profile?</Paragraph>
-        <CardActions>
-          <Button>Yes</Button>
-          <Button>No</Button>
-        </CardActions>
-      </Card>
-    </Section>
-  </Scene>
 </Story>
