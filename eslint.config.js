80bb3250b981a94a003cd230624f0ebd4c559794
Andrey Sitnik <andrey@sitnik.ru>
2024-03-02 17:05:25 +0100

    Move to ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import loguxSvelteConfig from '@logux/eslint-config/svelte'
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    ignores: ['web/dist/', 'web/storybook-static/']
+  },
+  ...loguxSvelteConfig,
+  {
+    files: ['web/stories/*.ts'],
+    rules: {
+      'no-console': 'off'
+    }
+  },
+  {
+    files: ['**/*.ts', '**/*.js', '**/*.svelte'],
+    rules: {
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          'groups': [
+            'side-effect',
+            ['side-effect-style', 'style'],
+            ['builtin-type', 'type', 'builtin', 'external', 'unknown'],
+            [
+              'internal-type',
+              'parent-type',
+              'sibling-type',
+              'index-type',
+              'internal',
+              'parent',
+              'sibling',
+              'index'
+            ],
+            ['object']
+          ],
+          'newlines-between': 'always',
+          'order': 'asc',
+          'type': 'alphabetical'
+        }
+      ]
+    }
+  }
+]
