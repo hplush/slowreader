711fd06e1216d9ec84ce2e3dd40b49485bd0344d
Andrey Sitnik <andrey@sitnik.ru>
2024-03-23 20:48:54 +0100

    Remove manifests and PNG icons from /ui/ since we need it only in root

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -3,6 +3,5 @@ import type { StorybookConfig } from '@storybook/svelte-vite'
 export default {
   addons: ['@storybook/addon-svelte-csf', '@storybook/addon-themes'],
   framework: '@storybook/svelte-vite',
-  staticDirs: ['../public'],
   stories: ['../stories/**/*.stories.svelte']
 } satisfies StorybookConfig
