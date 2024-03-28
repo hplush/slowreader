d91d0df247918b0cbc3b5bea830d7a5344fee799
Andrey Sitnik <andrey@sitnik.ru>
2024-03-28 20:54:02 +0100

    Use subprotocol from api/ to sync it with client

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,10 @@
 import { Server } from '@logux/server'
+import { SUBPROTOCOL } from '@slowreader/api'
 
 const server = new Server(
   Server.loadOptions(process, {
     fileUrl: import.meta.url,
-    subprotocol: '1.0.0',
+    subprotocol: SUBPROTOCOL,
     supports: '1.x'
   })
 )
