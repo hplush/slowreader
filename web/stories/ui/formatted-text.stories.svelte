c8ba59940027a6c134bb62cc522711c0e8b8a34a
upteran <upteran@gmail.com>
2024-03-31 22:23:13 +0200

    add(formatted-text): heading and layout tags styles and story

---
+++ web/stories/ui/formatted-text.stories.svelte
@@ -0,0 +1,55 @@
+<script context="module" lang="ts">
+  import FormattedText from '../../ui/formatted-text.svelte'
+
+  export const meta = {
+    component: FormattedText,
+    title: 'UI/FormattedText'
+  }
+</script>
+
+<script lang="ts">
+  import { Story } from '@storybook/addon-svelte-csf'
+
+  import Card from '../../ui/card.svelte'
+  //import Scene from '../scene.svelte'
+  import Section from '../section.svelte'
+
+  let headers =
+    '<h1>H1 Heading</h1><h2>H2 Heading</h2><h3>H3 Heading</h3><h4>H4 Heading</h4><h5>H5 Heading</h5><h6>H6 Heading</h6>'
+
+  let paragraph =
+    '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut culpa cumque, dolorum nobis quidem repellendus sunt! Amet assumenda aut deserunt, nest facilis nihil officiis pariatur quia quisquam sapiente tempora vitae.</p>'
+
+  let headersWithParagraphs = `<div><h1>Heading</h1>${paragraph}<h2>Heading 2</h2><p>${paragraph}</p><h3>Heading 3</h3><p>${paragraph}</p><h4>Heading 4</h4><p>${paragraph}</p><h5>Heading 4</h5><p>${paragraph}</p><h6>Heading 4</h6><p>${paragraph}</p></div>`
+
+  let layouts =
+    '<div></div><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A commodi eos recusandae. ' +
+    'Asperiores <span>debitis</span> dolore fugiat laboriosam neque ' +
+    'nostrum obcaecati odit tempora. A explicabo impedit perferendis quis sequi ' +
+    'voluptate. Quidem?</div><span>span text</span></div><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+    'Accusamus aliquid, amet aperiam assumenda ' +
+    'aut dicta, dolor earum explicabo ' +
+    'ipsa iste itaque laboriosam laudantium maxime qui quia quod, sed sint velit.</div>'
+  let images =
+    '<img src="https://media.dev.to/cdn-cgi/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fcl51qc17mia9ep2ujcdh.gif" alt="A scene from ..." loading="lazy" width="480" height="274" ><img src="https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwjyxxca8qxz6j0vc7av9.jpg" style="aspect-ratio: auto 1000 / 420;" width="1000" height="420"  alt="Cover">'
+  let getHtml = (content: string): string => `<html lang="en">${content}</html>`
+</script>
+
+<Story name="Base">
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(headers)}`} />
+      <FormattedText html={`${getHtml(headersWithParagraphs)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(layouts)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(images)}`} />
+    </Card>
+  </Section>
+</Story>
--- web/ui/formatted-text.svelte
+++ web/ui/formatted-text.svelte
@@ -17,7 +17,305 @@
 <div bind:this={node} class="formatted-text"></div>
 
 <style>
+  /** images */
   .formatted-text :global(img) {
     max-width: 100%;
+    height: auto;
+    padding: 0.3rem 0;
   }
+
+  /* .formatted-text :global(image) { */
+
+  /* } */
+
+  /** layout */
+
+  /* .formatted-text :global(div) { */
+
+  /* } */
+
+  /* .formatted-text :global(span) { */
+
+  /* } */
+
+  /** headers
+    step 1.2 between each heading
+  */
+  .formatted-text :global(h1) {
+    margin: 0.67rem 0;
+    font-size: 2.986rem;
+  }
+
+  .formatted-text :global(h2) {
+    margin: 0.83rem 0;
+    font-size: 2.488rem;
+  }
+
+  .formatted-text :global(h3) {
+    margin: 1rem 0;
+    font-size: 2.074rem;
+  }
+
+  .formatted-text :global(h4) {
+    margin: 1.33rem 0;
+    font-size: 1.728rem;
+  }
+
+  .formatted-text :global(h5) {
+    margin: 1.67rem 0;
+    font-size: 1.44rem;
+  }
+
+  .formatted-text :global(h6) {
+    margin: 1.95rem 0;
+    font-size: 1rem;
+  }
+
+  /* !** inline text formatting *! */
+
+  /* .formatted-text :global(a) { */
+
+  /* } */
+
+  /* .formatted-text :global(em) { */
+
+  /* } */
+
+  /* .formatted-text :global(mark) { */
+
+  /* } */
+
+  /* .formatted-text :global(p) { */
+
+  /* } */
+
+  /* .formatted-text :global(q) { */
+
+  /* } */
+
+  /* .formatted-text :global(strong) { */
+
+  /* } */
+
+  /* .formatted-text :global(sub) { */
+
+  /* } */
+
+  /* .formatted-text :global(sup) { */
+
+  /* } */
+
+  /* .formatted-text :global(small) { */
+
+  /* } */
+
+  /* .formatted-text :global(i) { */
+
+  /* } */
+
+  /* .formatted-text :global(b) { */
+
+  /* } */
+
+  /* .formatted-text :global(bdi) { */
+
+  /* } */
+
+  /* .formatted-text :global(bdo) { */
+
+  /* } */
+
+  /* .formatted-text :global(cite) { */
+
+  /* } */
+
+  /* .formatted-text :global(data) { */
+
+  /* } */
+
+  /* .formatted-text :global(del) { */
+
+  /* } */
+
+  /* .formatted-text :global(ins) { */
+
+  /* } */
+
+  /* .formatted-text :global(s) { */
+
+  /* } */
+
+  /* .formatted-text :global(samp) { */
+
+  /* } */
+
+  /* !** behavioral *! */
+
+  /* .formatted-text :global(br) { */
+
+  /* } */
+
+  /* .formatted-text :global(hr) { */
+
+  /* } */
+
+  /* !** lists *! */
+
+  /* .formatted-text :global(ul) { */
+
+  /* } */
+
+  /* .formatted-text :global(ol) { */
+
+  /* } */
+
+  /* .formatted-text :global(li) { */
+
+  /* } */
+
+  /* !** content *! */
+
+  /* .formatted-text :global(blockquote) { */
+
+  /* } */
+
+  /* .formatted-text :global(caption) { */
+
+  /* } */
+
+  /* .formatted-text :global(figcaption) { */
+
+  /* } */
+
+  /* .formatted-text :global(figure) { */
+
+  /* } */
+
+  /* .formatted-text :global(abbr) { */
+
+  /* } */
+
+  /* .formatted-text :global(address) { */
+
+  /* } */
+
+  /* .formatted-text :global(datalist) { */
+
+  /* } */
+
+  /* .formatted-text :global(dd) { */
+
+  /* } */
+
+  /* .formatted-text :global(dl) { */
+
+  /* } */
+
+  /* .formatted-text :global(details) { */
+
+  /* } */
+
+  /* .formatted-text :global(summary) { */
+
+  /* } */
+
+  /* .formatted-text :global(dfn) { */
+
+  /* } */
+
+  /* .formatted-text :global(kbd) { */
+
+  /* } */
+
+  /* .formatted-text :global(rt) { */
+
+  /* } */
+
+  /* .formatted-text :global(ruby) { */
+
+  /* } */
+
+  /* .formatted-text :global(rp) { */
+
+  /* } */
+
+  /* !** media *! */
+
+  /* .formatted-text :global(video) { */
+
+  /* } */
+
+  /* .formatted-text :global(audio) { */
+
+  /* } */
+
+  /* .formatted-text :global(source) { */
+
+  /* } */
+
+  /* !** @deprecated *! */
+
+  /* .formatted-text :global(center) { */
+
+  /* } */
+
+  /* .formatted-text :global(dir) { */
+
+  /* } */
+
+  /* !** code block *! */
+
+  /* .formatted-text :global(pre) { */
+
+  /* } */
+
+  /* .formatted-text :global(pre > code) { */
+
+  /* } */
+
+  /* .formatted-text :global(code) { */
+
+  /* } */
+
+  /* .formatted-text :global(p > code) { */
+
+  /* } */
+
+  /* !** table *! */
+
+  /* .formatted-text :global(table) { */
+
+  /* } */
+
+  /* .formatted-text :global(tbody) { */
+
+  /* } */
+
+  /* .formatted-text :global(td) { */
+
+  /* } */
+
+  /* .formatted-text :global(tfoot) { */
+
+  /* } */
+
+  /* .formatted-text :global(th) { */
+
+  /* } */
+
+  /* .formatted-text :global(thead) { */
+
+  /* } */
+
+  /* .formatted-text :global(tr) { */
+
+  /* } */
+
+  /* .formatted-text :global(col) { */
+
+  /* } */
+
+  /* .formatted-text :global(colgroup) { */
+
+  /* } */
 </style>
