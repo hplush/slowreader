057e8dce16c9b6dbbe7d159972505fd63f0d9a6c
upteran <upteran@gmail.com>
2024-04-01 22:31:35 +0200

    add(formatter-text): rest of tags with styles and stories

--- web/stories/ui/formatted-text.stories.svelte
+++ web/stories/ui/formatted-text.stories.svelte
@@ -32,6 +32,201 @@
     'ipsa iste itaque laboriosam laudantium maxime qui quia quod, sed sint velit.</div>'
   let images =
     '<img src="https://media.dev.to/cdn-cgi/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fcl51qc17mia9ep2ujcdh.gif" alt="A scene from ..." loading="lazy" width="480" height="274" ><img src="https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwjyxxca8qxz6j0vc7av9.jpg" style="aspect-ratio: auto 1000 / 420;" width="1000" height="420"  alt="Cover">'
+
+  let inlineTagsExample =
+    '<div>\n' +
+    '  <p><b>Bold</b> Text Example</p>\n' +
+    '  <p><i>Introduction</i></p>\n' +
+    '  <p><strong>Fish</strong> are a group of <em>aquatic</em> animals that first appeared during the <mark>Devonian</mark> period.</p>\n' +
+    '  <p><bdi>Types of Fish</bdi></p>\n' +
+    '  <p>There are three main types of fish: <b>bony fish</b>, <i>cartilaginous fish</i>, and <mark>jawless fish</mark>.</p>\n' +
+    '  <p><bdo>Characteristics of Fish</bdo></p>\n' +
+    '  <p>Fish are characterized by their <strong>gills</strong>, <em>fins</em>, and <mark>scales</mark>.</p>\n' +
+    '  <p><cite>Interesting Facts</cite></p>\n' +
+    '  <p><a href="">Some visited link</a></p>\n' +
+    '  <p>Did you know that the <data value="whale-shark">whale shark</data> is the largest fish in the world?</p>\n' +
+    '  <p><del>Conclusion</del></p>\n' +
+    '  <p><ins>Fish are fascinating creatures!</ins> They come in all shapes and sizes, from the tiny <small>paedocypris</small> to the massive <samp>whale shark</samp>.</p>\n' +
+    '  <p><sub>Subscript text</sub> and <sup>superscript text</sup> can also be used for scientific notation.</p>\n' +
+    '  <p>For more information, visit our <a href="https://example.com">website</a>.</p>\n' +
+    '</div>'
+
+  let lists =
+    '<div>\n  <h3>Unordered List</h3>\n  <ul>\n    <li>Fish</li>\n    <li>Crab</li>\n    <li>Shrimp</li>\n  </ul>\n\n  <h3>Ordered List</h3>\n  <ol>\n    <li>Seahorse</li>\n    <li>Octopus</li>\n    <li>Jellyfish</li>\n  </ol>\n</div>'
+
+  let contentTags =
+    ' <blockquote>Citation from a famous person</blockquote>\n' +
+    '  <table>\n' +
+    '    <caption>This is a caption for the table</caption>\n' +
+    '    <tr>\n' +
+    '      <td>Data 1</td>\n' +
+    '      <td>Data 2</td>\n' +
+    '    </tr>\n' +
+    '  </table>\n' +
+    '  <figure>\n' +
+    '    <img src="image.jpg" alt="Example image">\n' +
+    '    <figcaption>This is a figcaption for the image</figcaption>\n' +
+    '  </figure>\n' +
+    '  <p><abbr title="Hypertext Markup Language">HTML</abbr> is the standard markup language for documents designed to be displayed in a web browser.</p>\n' +
+    '  <address>\n' +
+    '    Written by John Doe.<br>\n' +
+    '    Visit us at:<br>\n' +
+    '    Example.com<br>\n' +
+    '    Box 564, Disneyland<br>\n' +
+    '    USA\n' +
+    '  </address>\n' +
+    '  <datalist id="browsers">\n' +
+    '    <option value="Internet Explorer">\n' +
+    '    <option value="Firefox">\n' +
+    '    <option value="Chrome">\n' +
+    '    <option value="Opera">\n' +
+    '    <option value="Safari">\n' +
+    '  </datalist>\n' +
+    '  <dl>\n' +
+    '    <dt>Coffee</dt>\n' +
+    '    <dd>Black hot drink</dd>\n' +
+    '    <dt>Milk</dt>\n' +
+    '    <dd>White cold drink</dd>\n' +
+    '  </dl>\n' +
+    '  <details>\n' +
+    '    <summary>Epcot Center</summary>\n' +
+    '    <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p>\n' +
+    '  </details>\n' +
+    '  <dfn title="Hypertext Markup Language">HTML</dfn>\n' +
+    '  <kbd>Ctrl+C</kbd>\n' +
+    '  <ruby>\n' +
+    '    漢 <rt>ㄏㄢˋ</rt>\n' +
+    '  </ruby>'
+  let mediaTags =
+    '<div>\n' +
+    '  <video controls>\n' +
+    '    <source src="movie.mp4" type="video/mp4">\n' +
+    '    <source src="movie.ogg" type="video/ogg">\n' +
+    '    Your browser does not support the video tag.\n' +
+    '  </video>\n' +
+    '\n' +
+    '  <audio controls>\n' +
+    '    <source src="horse.ogg" type="audio/ogg">\n' +
+    '    <source src="horse.mp3" type="audio/mpeg">\n' +
+    '    Your browser does not support the audio element.\n' +
+    '  </audio>\n' +
+    '</div>'
+
+  let code =
+    ' <pre><code>\n' +
+    '    function helloWorld() {\n' +
+    '      console.log("Hello, world!");\n' +
+    '    }\n' +
+    '  </code></pre>'
+
+  let codeInline =
+    '<p>This is a paragraph of text. Here is an example of inline code: <code>console.log("Hello, world!");</code></p>'
+
+  let table =
+    '<table>\n' +
+    '    <thead>\n' +
+    '      <tr>\n' +
+    '        <th>Name</th>\n' +
+    '        <th>Email</th>\n' +
+    '      </tr>\n' +
+    '    </thead>\n' +
+    '    <tbody>\n' +
+    '      <tr>\n' +
+    '        <td>John Doe</td>\n' +
+    '        <td>john.doe@example.com</td>\n' +
+    '      </tr>\n' +
+    '      <tr>\n' +
+    '        <td>Jane Doe</td>\n' +
+    '        <td>jane.doe@example.com</td>\n' +
+    '      </tr>\n' +
+    '    </tbody>\n' +
+    '  </table>'
+
+  let tableXl =
+    '<table>\n' +
+    '  <thead>\n' +
+    '    <tr>\n' +
+    '      <th>Header 1</th>\n' +
+    '      <th>Header 2</th>\n' +
+    '      <th>Header 3</th>\n' +
+    '      <th>Header 4</th>\n' +
+    '      <th>Header 5</th>\n' +
+    '      <th>Header 6</th>\n' +
+    '      <th>Header 7</th>\n' +
+    '    </tr>\n' +
+    '  </thead>\n' +
+    '  <tbody>\n' +
+    '    <tr>\n' +
+    '      <td>Data 1</td>\n' +
+    '      <td>Data 2</td>\n' +
+    '      <td>Data 3</td>\n' +
+    '      <td>Data 4</td>\n' +
+    '      <td>Data 5</td>\n' +
+    '      <td>Data 6</td>\n' +
+    '      <td>Data 7</td>\n' +
+    '    </tr>\n' +
+    '    <!-- Repeat the above row 6 more times -->\n' +
+    '    <tr>\n' +
+    '      <td>Data 1</td>\n' +
+    '      <td>Data 2</td>\n' +
+    '      <td>Data 3</td>\n' +
+    '      <td>Data 4</td>\n' +
+    '      <td>Data 5</td>\n' +
+    '      <td>Data 6</td>\n' +
+    '      <td>Data 7</td>\n' +
+    '    </tr>\n' +
+    '    <tr>\n' +
+    '      <td>Data 1</td>\n' +
+    '      <td>Data 2</td>\n' +
+    '      <td>Data 3</td>\n' +
+    '      <td>Data 4</td>\n' +
+    '      <td>Data 5</td>\n' +
+    '      <td>Data 6</td>\n' +
+    '      <td>Data 7</td>\n' +
+    '    </tr>\n' +
+    '    <tr>\n' +
+    '      <td>Data 1</td>\n' +
+    '      <td>Data 2</td>\n' +
+    '      <td>Data 3</td>\n' +
+    '      <td>Data 4</td>\n' +
+    '      <td>Data 5</td>\n' +
+    '      <td>Data 6</td>\n' +
+    '      <td>Data 7</td>\n' +
+    '    </tr>\n' +
+    '    <tr>\n' +
+    '      <td>Data 1</td>\n' +
+    '      <td>Data 2</td>\n' +
+    '      <td>Data 3</td>\n' +
+    '      <td>Data 4</td>\n' +
+    '      <td>Data 5</td>\n' +
+    '      <td>Data 6</td>\n' +
+    '      <td>Data 7</td>\n' +
+    '    </tr>\n' +
+    '    <tr>\n' +
+    '      <td>Data 1</td>\n' +
+    '      <td>Data 2</td>\n' +
+    '      <td>Data 3</td>\n' +
+    '      <td>Data 4</td>\n' +
+    '      <td>Data 5</td>\n' +
+    '      <td>Data 6</td>\n' +
+    '      <td>Data 7</td>\n' +
+    '    </tr>\n' +
+    '    <tr>\n' +
+    '      <td>Data 1</td>\n' +
+    '      <td>Data 2</td>\n' +
+    '      <td>Data 3</td>\n' +
+    '      <td>Data 4</td>\n' +
+    '      <td>Data 5</td>\n' +
+    '      <td>Data 6</td>\n' +
+    '      <td>Data 7</td>\n' +
+    '    </tr>\n' +
+    '  </tbody>\n' +
+    '  <tfoot>\n' +
+    '    <tr>\n' +
+    '      <td colspan="7">Footer information</td>\n' +
+    '    </tr>\n' +
+    '  </tfoot>\n' +
+    '</table>'
   let getHtml = (content: string): string => `<html lang="en">${content}</html>`
 </script>
 
@@ -50,6 +245,46 @@
   <Section>
     <Card>
       <FormattedText html={`${getHtml(images)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(inlineTagsExample)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(lists)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(contentTags)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(mediaTags)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(code)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(codeInline)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(table)}`} />
+    </Card>
+  </Section>
+  <Section>
+    <Card>
+      <FormattedText html={`${getHtml(tableXl)}`} />
     </Card>
   </Section>
 </Story>
--- web/ui/formatted-text.svelte
+++ web/ui/formatted-text.svelte
@@ -21,7 +21,8 @@
   .formatted-text :global(img) {
     max-width: 100%;
     height: auto;
-    padding: 0.3rem 0;
+    padding: 0.3em 0;
+    margin: 0 auto;
   }
 
   /* .formatted-text :global(image) { */
@@ -29,6 +30,10 @@
   /* } */
 
   /** layout */
+
+  .formatted-text :global(p) {
+    margin: 0 0 0.857em;
+  }
 
   /* .formatted-text :global(div) { */
 
@@ -39,113 +44,166 @@
   /* } */
 
   /** headers
-    step 1.2 between each heading
+    step 1.125 between each heading
   */
   .formatted-text :global(h1) {
-    margin: 0.67rem 0;
-    font-size: 2.986rem;
+    margin: 1em 0;
+    font-size: 1.476em;
   }
 
   .formatted-text :global(h2) {
-    margin: 0.83rem 0;
-    font-size: 2.488rem;
+    margin: 1em 0;
+    font-size: 1.429em;
   }
 
   .formatted-text :global(h3) {
-    margin: 1rem 0;
-    font-size: 2.074rem;
+    margin: 1em 0;
+    font-size: 1.296em;
   }
 
   .formatted-text :global(h4) {
-    margin: 1.33rem 0;
-    font-size: 1.728rem;
+    margin: 1em 0;
+    font-size: 1.215em;
   }
 
   .formatted-text :global(h5) {
-    margin: 1.67rem 0;
-    font-size: 1.44rem;
+    margin: 1em 0;
+    font-size: 1.138em;
   }
 
   .formatted-text :global(h6) {
-    margin: 1.95rem 0;
-    font-size: 1rem;
+    margin: 1em 0;
+    font-size: 1.067em;
   }
 
   /* !** inline text formatting *! */
+  .formatted-text :global(a) {
+    color: oklch(49.77% 0.224 271.1);
+    word-wrap: break-word;
+    transition: color 0.2s ease-in-out;
+  }
 
-  /* .formatted-text :global(a) { */
+  .formatted-text :global(a:visited) {
+    color: oklch(42.09% 0.193 328.36);
+  }
 
-  /* } */
+  .formatted-text :global(a:hover) {
+    color: oklch(37.73% 0.224 271.1);
+    transition: color 0.2s ease-in-out;
+  }
 
-  /* .formatted-text :global(em) { */
+  .formatted-text :global(a:active) {
+    color: oklch(57.97% 0.224 271.1);
+  }
 
-  /* } */
+  .formatted-text :global(em) {
+    font-style: italic;
+    font-weight: 600;
+  }
 
   /* .formatted-text :global(mark) { */
 
-  /* } */
+  /*  background-color: #ff0; */
 
-  /* .formatted-text :global(p) { */
+  /*  color: #000; */
 
   /* } */
 
   /* .formatted-text :global(q) { */
 
+  /*  quotes: '"' '"'; */
+
   /* } */
 
   /* .formatted-text :global(strong) { */
+
+  /*  font-weight: bold; */
 
   /* } */
 
   /* .formatted-text :global(sub) { */
 
+  /*  vertical-align: sub; */
+
+  /*  font-size: smaller; */
+
   /* } */
 
   /* .formatted-text :global(sup) { */
+
+  /*  vertical-align: super; */
+
+  /*  font-size: smaller; */
 
   /* } */
 
   /* .formatted-text :global(small) { */
 
+  /*  font-size: 80%; */
+
   /* } */
 
   /* .formatted-text :global(i) { */
+
+  /*  font-style: italic; */
 
   /* } */
 
   /* .formatted-text :global(b) { */
 
+  /*  font-weight: bold; */
+
   /* } */
 
   /* .formatted-text :global(bdi) { */
+
+  /*  direction: rtl; */
+
+  /*  unicode-bidi: isolate; */
 
   /* } */
 
   /* .formatted-text :global(bdo) { */
 
+  /*  direction: rtl; */
+
+  /*  unicode-bidi: bidi-override; */
+
   /* } */
 
   /* .formatted-text :global(cite) { */
+
+  /*  font-style: italic; */
 
   /* } */
 
   /* .formatted-text :global(data) { */
 
+  /*  color: #333; */
+
   /* } */
 
   /* .formatted-text :global(del) { */
+
+  /*  text-decoration: line-through; */
 
   /* } */
 
   /* .formatted-text :global(ins) { */
 
+  /*  text-decoration: underline; */
+
   /* } */
 
   /* .formatted-text :global(s) { */
 
+  /*  text-decoration: line-through; */
+
   /* } */
 
   /* .formatted-text :global(samp) { */
+
+  /*  font-family: monospace; */
 
   /* } */
 
@@ -161,35 +219,42 @@
 
   /* !** lists *! */
 
-  /* .formatted-text :global(ul) { */
+  .formatted-text :global(ul) {
+    padding-inline-start: 1.25em;
+    list-style-type: disc;
+  }
 
-  /* } */
+  .formatted-text :global(ol) {
+    padding-inline-start: 1.25em;
+    list-style-type: decimal;
+  }
 
-  /* .formatted-text :global(ol) { */
-
-  /* } */
-
-  /* .formatted-text :global(li) { */
-
-  /* } */
+  .formatted-text :global(li) {
+    margin-bottom: 0.625em;
+  }
 
   /* !** content *! */
 
-  /* .formatted-text :global(blockquote) { */
+  .formatted-text :global(blockquote) {
+    padding: 0.625em 1.25em;
+    margin: 0 0 1.25em;
+    border-inline-start: 0.3125em solid var(--border-color);
+  }
 
-  /* } */
+  .formatted-text :global(caption) {
+    padding: 0.625em;
+    font-style: italic;
+    text-align: center;
+  }
 
-  /* .formatted-text :global(caption) { */
+  .formatted-text :global(figcaption) {
+    font-style: italic;
+    text-align: center;
+  }
 
-  /* } */
-
-  /* .formatted-text :global(figcaption) { */
-
-  /* } */
-
-  /* .formatted-text :global(figure) { */
-
-  /* } */
+  .formatted-text :global(figure) {
+    margin: 1em 2.5em;
+  }
 
   /* .formatted-text :global(abbr) { */
 
@@ -203,9 +268,9 @@
 
   /* } */
 
-  /* .formatted-text :global(dd) { */
-
-  /* } */
+  .formatted-text :global(dd) {
+    margin-inline-start: 2.5em;
+  }
 
   /* .formatted-text :global(dl) { */
 
@@ -241,13 +306,18 @@
 
   /* !** media *! */
 
-  /* .formatted-text :global(video) { */
+  .formatted-text :global(video) {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    margin: 0 auto;
+  }
 
-  /* } */
-
-  /* .formatted-text :global(audio) { */
-
-  /* } */
+  .formatted-text :global(audio) {
+    display: block;
+    width: 100%;
+    margin: 1em 0;
+  }
 
   /* .formatted-text :global(source) { */
 
@@ -265,57 +335,63 @@
 
   /* !** code block *! */
 
-  /* .formatted-text :global(pre) { */
+  .formatted-text :global(pre) {
+    padding: 1em;
+    margin: 0.8em 0;
+    overflow-x: auto;
+    background-color: var(--field-color);
+    border-radius: 4px;
+    box-shadow: var(--field-shadow);
+  }
 
-  /* } */
+  .formatted-text :global(code) {
+    padding: 0.3em;
+    line-height: 1.8;
+    color: var(--text-color);
+    background: var(--field-color);
+    border-radius: var(--radius);
+  }
 
-  /* .formatted-text :global(pre > code) { */
+  .formatted-text :global(pre > code) {
+    max-width: 100%;
+    padding: 0.1em 0.25em;
+    font-size: 0.89em;
+    background: transparent;
+  }
 
-  /* } */
-
-  /* .formatted-text :global(code) { */
-
-  /* } */
-
-  /* .formatted-text :global(p > code) { */
-
-  /* } */
+  .formatted-text :global(p code) {
+    padding: 0.3em;
+    font-size: 80%;
+    border-radius: var(--radius);
+  }
 
   /* !** table *! */
 
-  /* .formatted-text :global(table) { */
+  .formatted-text :global(table) {
+    width: 100%;
+    margin: 1em 0;
+    border-collapse: collapse;
+  }
 
-  /* } */
+  .formatted-text :global(td) {
+    padding: 0.8em;
+    border: 1px solid var(--border-color);
+  }
 
-  /* .formatted-text :global(tbody) { */
+  .formatted-text :global(th) {
+    padding: 0.8em;
+    border: 1px solid var(--border-color);
+  }
 
-  /* } */
+  .formatted-text :global(tr) {
+    border-bottom: 1px solid var(--border-color);
+  }
 
-  /* .formatted-text :global(td) { */
+  .formatted-text :global(col) {
+    border: 1px solid var(--border-color);
+  }
 
-  /* } */
-
-  /* .formatted-text :global(tfoot) { */
-
-  /* } */
-
-  /* .formatted-text :global(th) { */
-
-  /* } */
-
-  /* .formatted-text :global(thead) { */
-
-  /* } */
-
-  /* .formatted-text :global(tr) { */
-
-  /* } */
-
-  /* .formatted-text :global(col) { */
-
-  /* } */
-
-  /* .formatted-text :global(colgroup) { */
-
-  /* } */
+  .formatted-text :global(colgroup) {
+    border: 1px solid var(--border-color);
+  }
 </style>
