a85512ee2f81c09db3739d860a72cb41e1b55f1c
ilyatitovich <ilyatitovich@gmail.com>
2024-04-01 12:59:18 +0200

    Add GitHub Action for for testing .md files

--- .remarkrc.js
+++ .remarkrc.js
@@ -2,9 +2,5 @@
 // Add the name of the plugin you want to use to the list of plugins.
 
 export default {
-  plugins: [
-    'remark-lint-no-dead-urls',
-    'remark-validate-links',
-    'remark-lint-code-block-style'
-  ]
+  plugins: ['remark-lint-no-dead-urls', 'remark-validate-links']
 }
