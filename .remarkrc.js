960b12ef01f3cb8cd91c60bdeed6c9420043f1f9
ilyatitovich <ilyatitovich@gmail.com>
2024-04-02 16:44:57 +0200

    Updated description of .remark.js

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,5 +1,4 @@
-// Configuration file used to set up a tool to find mistakes in .md files.
-// Add the name of the plugin you want to use to the list of plugins.
+// Configuration file used to find popular mistakes in .md files.
 
 export default {
   plugins: ['remark-lint-no-dead-urls', 'remark-validate-links']
