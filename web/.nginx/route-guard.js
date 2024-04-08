46295b891ebabcd9cb23f7f8f72f35c0a2d3c322
Gregory N! <nmailster@gmail.com>
2024-04-09 02:09:46 +0700

    Improvements for preview server configuration (#154)
    * Return HTML on apps routes (#137)
    
    * Update web/README.md
    
    Co-authored-by: Andrey Sitnik <andrey@sitnik.ru>
    
    * In preview server config use module import instead of json parsing for router regexes (#137)
    
    * In preview server use static error pages templates (#137)
    
    * Enable assets cache on preview server
    
    * Enable gzip compression on preview server
    
    ---------
    
    Co-authored-by: Andrey Sitnik <andrey@sitnik.ru>

---
+++ web/.nginx/route-guard.js
@@ -0,0 +1,28 @@
+// Code in this file is in NJS language https://nginx.org/en/docs/njs/
+
+// eslint-disable-next-line
+import rawRegExps from '/etc/nginx/njs/routes.js'
+
+const ROUTE_REGEXPS = rawRegExps.map(
+  route => new RegExp(route.source, route.flags)
+)
+
+/**
+ * Return 404 status if requested path is not supported by application.
+ * @param r - Nginx HTTP Request https://nginx.org/en/docs/njs/reference.html#http
+ */
+async function check(r) {
+  r.headersOut['Content-Type'] = 'text/html'
+
+  let requestPath = r.uri.split('?')[0]
+  let routeExists = ROUTE_REGEXPS.find(route => route.test(requestPath))
+
+  if (routeExists) {
+    let appIndex = await r.subrequest('/index.html', r.variables.args)
+    return r.return(200, appIndex.responseText)
+  } else {
+    return r.return(404)
+  }
+}
+
+export default { check }
