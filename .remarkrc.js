15376f4eb930fc1b07e00557a247b7f6198446b2
ilyatitovich <ilyatitovich@gmail.com>
2024-04-08 18:42:32 +0200

    Installed a custom plugin for checking Markdown file headings

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,14 +1,9 @@
 // Configuration file used to find popular mistakes in .md files.
 
-import remarkLintTitleCapitalization from './remark-custom-rules/title-capitalization/remarkLintTitleCapitalization.js'
-
 export default {
   plugins: [
     'remark-lint-no-dead-urls',
     'remark-validate-links',
-    [
-      remarkLintTitleCapitalization,
-      { exceptionWords: { '2FA': true, 'GitHub': true } }
-    ]
+    'remark-lint-heading-capitalization'
   ]
 }
