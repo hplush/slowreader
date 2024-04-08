5f3c7089a13f53d5070942c7ab656e3f73045576
ilyatitovich <ilyatitovich@gmail.com>
2024-04-08 19:08:49 +0200

    Add custom remark plugin for checking headings capitalization

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,5 +1,9 @@
 // Configuration file used to find popular mistakes in .md files.
 
 export default {
-  plugins: ['remark-lint-no-dead-urls', 'remark-validate-links']
+  plugins: [
+    'remark-lint-no-dead-urls',
+    'remark-validate-links',
+    'remark-lint-heading-capitalization'
+  ]
 }
