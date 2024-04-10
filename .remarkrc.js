efc3a75e3402ab9de27e0cf24e881f63fb9d3f94
ilyatitovich <ilyatitovich@gmail.com>
2024-04-10 15:03:09 +0200

    Add custom remark plugin for checking code block inside list

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,9 +1,11 @@
 // Configuration file used to find popular mistakes in .md files.
+import remarkLintCodeBlockSplitList from './remark/remark-lint-code-block-split-list/index.js'
 
 export default {
   plugins: [
     'remark-lint-no-dead-urls',
     'remark-validate-links',
-    'remark-lint-heading-capitalization'
+    'remark-lint-heading-capitalization',
+    remarkLintCodeBlockSplitList
   ]
 }
