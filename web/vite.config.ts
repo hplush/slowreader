3395437ef3f135813860182a4da80367cdb615b0
daniil <Daniil Makhov>
2024-04-14 00:30:10 +0300

    Add campfire.svg icon to /web/main folder, change favicon for localhost and build to separate dev environment with prodution. Now for production we use icon.svg in index.html entry point and for development we use campfire.svg in index.html

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,6 +10,18 @@ export default defineConfig(() => ({
     svelte(),
     nodeResolve({
       extensions: ['.js', '.ts']
-    })
+    }),
+    {
+      name: 'html-transform',
+      enforce: 'pre' as const,
+      transformIndexHtml: (html: string) => {
+        return html.replace(
+          '/main/icon.svg',
+          process.env.NODE_ENV === 'development'
+            ? '/main/campfire.svg'
+            : '/main/icon.svg'
+        )
+      }
+    }
   ]
 }))
