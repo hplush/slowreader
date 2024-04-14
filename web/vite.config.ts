9bfbf99d9cd25846c8d25024fb5e85db0a90a6a3
Daniil Makhov <73839778+Edward-Leopold@users.noreply.github.com>
2024-04-14 19:57:09 +0300

    Add favicon for development (#162)
    * Add campfire.svg icon to /web/main folder, change favicon for localhost and build to separate dev environment with prodution. Now for production we use icon.svg in index.html entry point and for development we use campfire.svg in index.html
    
    * revert changes in style section
    
    * return deleted .ico link in index.html
    
    * add STAGING variable for github action workflow to set STAGING variable when pnpm build is running
    
    * add comment at vite.config.ts
    
    * swap enforce and name fields in vite.config.ts
    
    * comments was removed from vite.config.ts
    
    * move campfire.svg from web/main to web/public, rewrite transformIndexHtml function in vite.config.ts
    
    * favicon campfire.svg was moved web/public folder
    
    ---------
    
    Co-authored-by: daniil <Daniil Makhov>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,6 +10,25 @@ export default defineConfig(() => ({
     svelte(),
     nodeResolve({
       extensions: ['.js', '.ts']
-    })
+    }),
+    {
+      enforce: 'pre',
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        if (process.env.NODE_ENV === 'development' || process.env.STAGING) {
+          return html
+            .replace(
+              '<link rel="icon" href="/favicon.ico" sizes="32x32" />',
+              ''
+            )
+            .replace(
+              /<link rel="icon" href="[^"]+" type="image\/svg\+xml" \/>/,
+              '<link rel="icon" href="/campfire.svg" type="image/svg+xml" />'
+            )
+        } else {
+          return html
+        }
+      }
+    }
   ]
 }))
