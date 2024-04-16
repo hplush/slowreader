7f33ce05c5accf181f7a79366a15cdf2e120e8d2
Andrey Sitnik <andrey@sitnik.ru>
2024-04-16 12:27:01 +0200

    Clean up code

--- core/loader/json-feed.ts
+++ core/loader/json-feed.ts
@@ -5,16 +5,32 @@ import type { Loader } from './index.js'
 import { findAnchorHrefs, findLinksByType, toTime } from './utils.js'
 
 // https://www.jsonfeed.org/version/1.1/
-export type Author = {
+interface JsonFeed {
+  /** deprecated from 1.1 version */
+  author?: JsonFeedAuthor
+  authors?: JsonFeedAuthor[]
+  description?: string
+  favicon?: string
+  feed_url?: string
+  home_page_url?: string
+  icon?: string
+  items: JsonFeedItem[]
+  next_url?: string
+  title: string
+  user_comment?: string
+  version: string
+}
+
+interface JsonFeedAuthor {
   avatar?: string
   name?: string
   url?: string
 }
 
-export type Item = {
+interface JsonFeedItem {
   /** deprecated from 1.1 version */
-  author?: Author
-  authors?: Author[]
+  author?: JsonFeedAuthor
+  authors?: JsonFeedAuthor[]
   banner_image?: string
   content_html?: string
   content_text?: string
@@ -29,25 +45,21 @@ export type Item = {
   url?: string
 }
 
-export type JsonFeed = {
-  /** deprecated from 1.1 version */
-  author?: Author
-  authors?: Author[]
-  description?: string
-  favicon?: string
-  feed_url?: string
-  home_page_url?: string
-  icon?: string
-  items: Item[]
-  next_url?: string
-  title: string
-  user_comment?: string
-  version: string
-}
-
 type ValidationRules = {
   [key: string]: (value: unknown) => boolean
 }
+
+const JSON_FEED_VERSIONS = ['1', '1.1']
+
+const JSON_FEED_VALIDATORS = {
+  items: value => Array.isArray(value),
+  title: value => typeof value === 'string',
+  version: value => {
+    if (typeof value !== 'string' || !value.includes('jsonfeed')) return false
+    let version = value.split('/').pop()
+    return JSON_FEED_VERSIONS.includes(version!)
+  }
+} satisfies ValidationRules
 
 function isObject(value: unknown): value is Record<string, unknown> {
   return value !== null && typeof value === 'object' && !Array.isArray(value)
@@ -69,7 +81,8 @@ function validate<ValidatedType>(
     if (!(field in value) || !rules[field]!(value[field])) {
       // eslint-disable-next-line no-console
       console.error(
-        `JSON feed field '${field}' is not valid with value: ${stringify(value[field])}`
+        `JSON feed field '${field}' is not valid with value`,
+        stringify(value[field])
       )
       return false
     }
@@ -78,21 +91,9 @@ function validate<ValidatedType>(
   return true
 }
 
-const JSON_FEED_VERSIONS = ['1', '1.1']
-
-let jsonFeedValidationRules: ValidationRules = {
-  items: (value): boolean => Array.isArray(value),
-  title: (value): boolean => typeof value === 'string',
-  version: (value): boolean => {
-    if (typeof value !== 'string' || !value.includes('jsonfeed')) return false
-    let version = value.split('/').pop()
-    return JSON_FEED_VERSIONS.includes(version!)
-  }
-}
-
 function parsePosts(text: TextResponse): OriginPost[] {
   let parsedJson = text.parseJson()
-  if (!validate<JsonFeed>(parsedJson, jsonFeedValidationRules)) return []
+  if (!validate<JsonFeed>(parsedJson, JSON_FEED_VALIDATORS)) return []
 
   return parsedJson.items.map(item => ({
     full: (item.content_html || item.content_text) ?? undefined,
@@ -131,7 +132,7 @@ export const jsonFeed: Loader = {
 
   isMineText(text) {
     let parsedJson = text.parseJson()
-    if (validate<JsonFeed>(parsedJson, jsonFeedValidationRules)) {
+    if (validate<JsonFeed>(parsedJson, JSON_FEED_VALIDATORS)) {
       return parsedJson.title
     }
     return false
