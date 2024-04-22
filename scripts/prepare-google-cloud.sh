1cfa9349707316390004c8bc49766300719ae16e
Andrey Sitnik <andrey@sitnik.ru>
2024-04-22 22:29:04 +0200

    Clean up Google CLoud auth provider name

--- scripts/prepare-google-cloud.sh
+++ scripts/prepare-google-cloud.sh
@@ -40,11 +40,11 @@ gcloud iam workload-identity-pools create "github" \
   --project=$PROJECT_ID \
   --location="global" \
   --display-name="GitHub Actions Pool"
-gcloud iam workload-identity-pools providers create-oidc "slowreader" \
+gcloud iam workload-identity-pools providers create-oidc "hplush" \
   --project=$PROJECT_ID \
   --location="global" \
   --workload-identity-pool="github" \
-  --display-name="GitHub Slow Reader Provider" \
+  --display-name="GitHub hplush Ogranization" \
   --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository,attribute.repository_owner=assertion.repository_owner" \
   --attribute-condition="assertion.repository_owner == 'hplush'" \
   --issuer-uri="https://token.actions.githubusercontent.com"
