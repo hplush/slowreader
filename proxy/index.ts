eaf62b945fcca7d4c4e99453f7ddcd62989877b5
Andrey Sitnik <andrey@sitnik.ru>
2024-04-28 14:29:30 +0200

    Use staging/production Origin checks in proxy

--- proxy/index.ts
+++ proxy/index.ts
@@ -4,14 +4,20 @@ import { createProxyServer } from './proxy.js'
 
 const PORT = 5284
 
+let allowsFrom: RegExp[]
+if (process.env.NODE_ENV !== 'production') {
+  allowsFrom = [/^http:\/\/localhost:5173/]
+} else if (process.env.STAGING) {
+  allowsFrom = [
+    /^https:\/\/dev.slowreader.app/,
+    /^https:\/\/(preview-\d+---)?staging-jfj4bxwwxq-ew.a.run.app/
+  ]
+} else {
+  allowsFrom = [/^https:\/\/slowreader.app/]
+}
+
 let proxy = createProxyServer({
-  allowsFrom:
-    process.env.NODE_ENV === 'production'
-      ? [
-          /^https:\/\/dev.slowreader.app/,
-          /^https:\/\/(preview-\d+---)?staging-jfj4bxwwxq-ew.a.run.app/
-        ]
-      : [/^http:\/\/localhost:5173/],
+  allowsFrom,
   maxSize: 10 * 1024 * 1024,
   timeout: 2500
 })
