ee310492e9559acd36dc8df3f3cf7a75aa80f2c3
Andrey Sitnik <andrey@sitnik.ru>
2024-04-28 22:24:50 +0200

    Fix routes export to fix difference between JS and nginx in %2F processing

--- web/scripts/export-routes.ts
+++ web/scripts/export-routes.ts
@@ -1,5 +1,7 @@
 // Save web client pages for server to return HTML on GET request to this routes
 
+// eslint-disable
+
 import { writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 
@@ -7,15 +9,21 @@ import { pathRouter } from '../stores/router.js'
 
 const ROUTES = join(import.meta.dirname, '../routes.regexp')
 
+const FIXES: Record<string, string> = {
+  '^\\/feeds\\/add(?:\\/([^/]+))?$': '^/feeds/add(/|$)'
+}
+
 function removeNamingGroup(regexp: string): string {
-  return regexp.replace(/\(\?<(\w+?)>/g, '(')
+  return regexp.replace(/\(\?<(\w+?)>\(\?<=\\\/\)/g, '(')
+}
+
+function fixRegexp(regexp: string): string {
+  return FIXES[regexp] || regexp
 }
 
 writeFileSync(
   ROUTES,
-  '(' +
-    pathRouter.routes
-      .map(([, regexp]) => removeNamingGroup(regexp.source))
-      .join('|') +
-    ')'
+  pathRouter.routes
+    .map(([, regexp]) => fixRegexp(removeNamingGroup(regexp.source)))
+    .join('|')
 )
