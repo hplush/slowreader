4b3b8604c3392a80b20b3c7de1967cba169b9aa7
Arthur Baileys Li <arthurlitovko@gmail.com>
2024-05-26 14:28:37 +0300

    Write stories for fast posts

---
+++ web/stories/pages/posts/fast-mock-env.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {
+    fastLoading,
+    fastPosts,
+    nextFastSince,
+    setFastPostsPerPage,
+    openedFastPost
+  } from '@slowreader/core'
+  import type { FastEntry, PostValue } from '@slowreader/core'
+
+  import Fast from '../../../pages/fast.svelte'
+
+  export let posts: FastEntry[] = []
+  export let openedPost: PostValue | undefined = undefined
+  export let showNextButton = false
+  // @ts-expect-error
+  fastPosts.set(posts)
+  // @ts-expect-error
+  nextFastSince.set(showNextButton ? 3 : undefined)
+  // @ts-expect-error
+  openedFastPost.set(openedPost)
+</script>
+
+<Fast />
---
+++ web/stories/pages/posts/fast.stories.svelte
@@ -0,0 +1,26 @@
+<script context="module" lang="ts">
+  import MockedFast from './fast-mock-env.svelte'
+  import { Story } from '@storybook/addon-svelte-csf'
+  import { mockedFast } from './mocks.js'
+
+  export const meta = {
+    component: MockedFast,
+    title: 'Pages/Post/Fast'
+  }
+</script>
+
+<Story name="Empty" parameters={{ layout: 'fullscreen' }}>
+  <MockedFast />
+</Story>
+
+<Story name="Have posts" parameters={{ layout: 'fullscreen' }}>
+  <MockedFast posts={mockedFast.slice(0, 3)} />
+</Story>
+
+<Story name="Next button" parameters={{ layout: 'fullscreen' }}>
+  <MockedFast posts={mockedFast} showNextButton />
+</Story>
+
+<Story name="Opened post" parameters={{ layout: 'fullscreen' }}>
+  <MockedFast posts={mockedFast.slice(0, 3)} openedPost={mockedFast[0].post} />
+</Story>
