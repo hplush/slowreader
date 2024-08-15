082549a2571f7ff322eb8e1ee5a78e4a83478458
Andrey Sitnik <andrey@sitnik.ru>
2024-08-15 00:30:59 +0000

    Avoid hacks with JSON files

---
+++ extension/manifest.config.ts
@@ -0,0 +1,21 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+
+const URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://*.slowreader.app/*'
+    : 'http://localhost:5173/*'
+
+export default defineManifest(async () => ({
+  background: {
+    service_worker: 'src/background.ts',
+    type: 'module'
+  },
+  description: 'Fetch data from websites for dev.slowreader.app',
+  externally_connectable: {
+    matches: [URL]
+  },
+  host_permissions: [URL],
+  manifest_version: 3,
+  name: 'Slowreader Extension',
+  version: '0.0.1'
+}))
