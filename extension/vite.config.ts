293b3256fd80b2f364ba91e329284c2cd23f6b93
Andrey Sitnik <andrey@sitnik.ru>
2024-08-15 00:13:50 +0000

    Clean up code style

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,15 +1,8 @@
 import { crx } from '@crxjs/vite-plugin'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
 import { defineConfig } from 'vite'
 
 import manifest from './manifest.json'
 
-const outDir = path.resolve(__dirname, 'dist')
-
 export default defineConfig({
-  build: {
-    outDir
-  },
-  plugins: [react(), crx({ manifest })]
+  plugins: [crx({ manifest })]
 })
