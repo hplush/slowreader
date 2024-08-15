3100b524fd5ed6ef515361f9368fa7dfdd6f23b5
Andrey Sitnik <andrey@sitnik.ru>
2024-08-15 00:25:40 +0000

    Fix CI

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,8 +1,8 @@
-import { crx } from '@crxjs/vite-plugin'
+import { crx, type ManifestV3Export } from '@crxjs/vite-plugin'
 import { defineConfig } from 'vite'
 
 import manifest from './manifest.json'
 
 export default defineConfig({
-  plugins: [crx({ manifest })]
+  plugins: [crx({ manifest: manifest as ManifestV3Export })]
 })
