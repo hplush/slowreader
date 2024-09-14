199c675b27b8ce9e7108e5bcb7e37ce6bfd4f524
Andrey Sitnik <andrey@sitnik.ru>
2024-09-14 14:54:46 +0000

    Move to custom PostCSS processor for Svelte

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,33 @@
-import { sveltePreprocess } from 'svelte-preprocess'
+import postcss from 'postcss'
+import loadPostcssConfig from 'postcss-load-config'
 
+let { plugins } = await loadPostcssConfig({}, import.meta.filename)
+let processor = postcss(plugins)
+
+/**
+ * @typedef {Object} SvelteConfig
+ * @property {import('svelte/compiler').PreprocessorGroup} preprocess
+ */
+
+/**
+ * @type {SvelteConfig}
+ */
 export default {
-  preprocess: sveltePreprocess()
+  preprocess: {
+    async style({ content, filename }) {
+      let { css, map, messages } = await processor.process(content, {
+        from: filename,
+        map: { annotation: false, inline: false }
+      })
+      let dependencies = messages.reduce((list, msg) => {
+        if (msg.type === 'dependency') list.push(msg.file)
+        return list
+      }, [])
+      return {
+        code: css,
+        dependencies,
+        map
+      }
+    }
+  }
 }
