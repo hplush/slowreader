55db810658b61cc4735e5a3d95ef3484201c69b2
Andrey Sitnik <andrey@sitnik.ru>
2024-09-15 14:37:07 +0200

    Add Drizzle Studio

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,7 +1,13 @@
+// Config to drizzle-kit CLI
+
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
+  dbCredentials: {
+    url: './db/pgdata'
+  },
   dialect: 'postgresql',
+  driver: 'pglite',
   out: './db/migrations',
   schema: './db/schema.ts'
 })
