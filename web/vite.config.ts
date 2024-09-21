4cb389ffe677a418184e6ea0ed2115ecce9ea8bc
Andrey Sitnik <andrey@sitnik.ru>
2024-09-21 14:15:29 +0000

    Separated icons for local and staging

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,6 +2,15 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 
+function replaceIcon(html: string, icon: string): string {
+  return html
+    .replace('<link rel="icon" href="/favicon.ico" sizes="32x32" />', '')
+    .replace(
+      /<link rel="icon" href="[^"]+" type="image\/svg\+xml" \/>/,
+      `<link rel="icon" href="/${icon}.svg" type="image/svg+xml" />`
+    )
+}
+
 export default defineConfig(() => ({
   build: {
     assetsInlineLimit: 0,
@@ -16,16 +25,10 @@ export default defineConfig(() => ({
       enforce: 'pre',
       name: 'html-transform',
       transformIndexHtml(html) {
-        if (process.env.NODE_ENV === 'development' || process.env.STAGING) {
-          return html
-            .replace(
-              '<link rel="icon" href="/favicon.ico" sizes="32x32" />',
-              ''
-            )
-            .replace(
-              /<link rel="icon" href="[^"]+" type="image\/svg\+xml" \/>/,
-              '<link rel="icon" href="/campfire.svg" type="image/svg+xml" />'
-            )
+        if (process.env.NODE_ENV === 'development') {
+          return replaceIcon(html, 'icon-dev')
+        } else if (process.env.STAGING) {
+          return replaceIcon(html, 'icon-staging')
         } else {
           return html
         }
