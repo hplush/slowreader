72e2cb84431186cdeda14c9074515c6fc5a3d96a
Andrey Sitnik <andrey@sitnik.ru>
2025-03-18 21:31:20 +0100

    Reduce code duplication in shell scripts

--- web/scripts/run-image.sh
+++ web/scripts/run-image.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 # Test real production environment with Podman or Docker
 
-set -e
-
-ERROR='\033[0;31m'
-WARNING='\033[0;33m'
-OK='\033[1;32m'
-NC='\033[0m' # No Color
-
-command_exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-
-build_and_run() {
-  BUILD_OUTPUT=$($1 build . 2>&1) || {
-    echo -e "${ERROR}Build failed:${NC}\n$BUILD_OUTPUT"
-    exit 1
-  }
-  IMAGE_ID=$(echo "$BUILD_OUTPUT" | tail -1)
-  SIZE=$($1 image inspect "$IMAGE_ID" --format='{{.Size}}' | \
-    awk '{printf "%d MB", $1/1024/1024}')
-  echo -e "${WARNING}Image size: ${SIZE}${NC}"
-  echo -e "${OK}Web server is running on http://localhost:8080${NC}"
-  $1 run --rm -p 8080:8080 -e PORT=8080 -it $IMAGE_ID
-}
-
-if command_exists podman; then
-  build_and_run podman
-elif command_exists docker; then
-  build_and_run docker
-else
-  echo -e "${ERROR}Install Podman or Docker${NC}"
-  exit 1
-fi
+source "$(dirname "$0")/../../scripts/image-utils.sh"
+build_and_run 8080
