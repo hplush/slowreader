c23905fc5d34567c7d93499072e7cc2e9859324e
Andrey Sitnik <andrey@sitnik.ru>
2025-03-22 22:12:09 +0100

    Reduce image size by moving to scratch base (#296)
    * Collect debug data
    
    * Use alpine for all images
    
    * Add a way to track container image size
    
    * Better error output in scrips
    
    * Reduce code duplication in shell scripts
    
    * Move to scratch base for Node.js images
    
    * Remove debug code
    
    * Fix rebase

--- proxy/scripts/run-image.sh
+++ proxy/scripts/run-image.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
-# Use Podman instead of Docker if it is avaiable to run image in production mode
+# Test real production environment with Podman or Docker
 
-ERROR='\033[0;31m'
-NC='\033[0m' # No Color
-
-command_exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-
-build_and_run() {
-  IMAGE_ID=$($1 build . | tail -1)
-  $1 run --rm -p 5284:5284 \
-    -e PORT=5284 -e PROXY_ORIGIN=^http:\\/\\/localhost:5173$ \
-    -it $IMAGE_ID
-}
-
-if command_exists podman; then
-  build_and_run podman
-elif command_exists docker; then
-  build_and_run docker
-else
-  echo -e "${ERROR}Install Podman or Docker${NC}"
-  exit 1
-fi
+source "$(dirname "$0")/../../scripts/image-utils.sh"
+build_and_run 5284 "-e PROXY_ORIGIN=^http:\\/\\/localhost:5173$"
