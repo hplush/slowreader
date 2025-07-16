b9012b101e0d2b3c85ed8bb65c0edbcd18f484ed
Andrey Sitnik <andrey@sitnik.ru>
2025-07-16 22:21:30 +0000

    Try to fix pglite on staging

--- server/db/index.ts
+++ server/db/index.ts
@@ -5,9 +5,7 @@ import { drizzle as devDrizzle } from 'drizzle-orm/pglite'
 import { migrate as devMigrate } from 'drizzle-orm/pglite/migrator'
 import { drizzle as prodDrizzle } from 'drizzle-orm/postgres-js'
 import { migrate as prodMigrate } from 'drizzle-orm/postgres-js/migrator'
-import { Buffer } from 'node:buffer'
-import { existsSync } from 'node:fs'
-import { readFile, writeFile } from 'node:fs/promises'
+import { access, constants } from 'node:fs/promises'
 import { join } from 'node:path'
 import postgres from 'postgres'
 
@@ -20,32 +18,11 @@ const MIGRATE_CONFIG: MigrationConfig = {
 }
 
 let drizzle: PgDatabase<PgQueryResultHKT, typeof schema>
-if (
-  config.db.startsWith('memory:') ||
-  config.db.startsWith('file:') ||
-  config.db.startsWith('dump:')
-) {
-  let pglite: PGlite
-  if (config.db.startsWith('dump:')) {
-    let path = config.db.slice(5)
-    if (existsSync(path)) {
-      let dump = await readFile(path)
-      pglite = new PGlite({
-        loadDataDir: new Blob([dump], { type: 'application/x-tar' })
-      })
-    } else {
-      pglite = new PGlite()
-    }
-    async function dumpDb(): Promise<void> {
-      let blob = await pglite.dumpDataDir('none')
-      await writeFile(path, Buffer.from(await blob.arrayBuffer()), {
-        encoding: 'binary'
-      })
-    }
-    setInterval(dumpDb, 1 * 1000)
-  } else {
-    pglite = new PGlite(config.db)
+if (config.db.startsWith('memory:') || config.db.startsWith('file:')) {
+  if (config.db.startsWith('file:')) {
+    await access(config.db.slice(5), constants.R_OK | constants.W_OK)
   }
+  let pglite = new PGlite(config.db, { debug: 1 })
   let drizzlePglite = devDrizzle(pglite, { schema })
   await devMigrate(drizzlePglite, MIGRATE_CONFIG)
   drizzle = drizzlePglite
