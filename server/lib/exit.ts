827ba81561a4f082069fa045f539bd1397fd8526
Andrey Sitnik <andrey@sitnik.ru>
2025-07-17 15:04:07 +0000

    Fix test coverage

--- server/lib/exit.ts
+++ server/lib/exit.ts
@@ -4,11 +4,13 @@ export function onExit(cb: () => void): void {
   listeners.push(cb)
 }
 
+/* c8 ignore start */
 function exit(): void {
   for (let listener of listeners) {
     listener()
   }
 }
+/* c8 ignore end */
 
 process.on('SIGTERM', exit)
 process.on('SIGINT', exit)
