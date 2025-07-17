a41f3f971a3fb4b0ebaed8582f63ce8bedfd9a3d
Andrey Sitnik <andrey@sitnik.ru>
2025-07-17 19:58:32 +0000

    Another way to fix test coverage

--- server/lib/exit.ts
+++ server/lib/exit.ts
@@ -4,13 +4,11 @@ export function onExit(cb: () => void): void {
   listeners.push(cb)
 }
 
-/* c8 ignore start */
 function exit(): void {
   for (let listener of listeners) {
     listener()
   }
 }
-/* c8 ignore end */
 
 process.on('SIGTERM', exit)
 process.on('SIGINT', exit)
