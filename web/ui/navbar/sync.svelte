7933dd37b538eb66344e33268a5ec4b043b3289f
Andrey Sitnik <andrey@sitnik.ru>
2025-10-13 14:38:02 +0000

    Add a link from sync status to Profile page to see sync settings and details

--- web/ui/navbar/sync.svelte
+++ web/ui/navbar/sync.svelte
@@ -14,6 +14,8 @@
     navbarMessages as t
   } from '@slowreader/core'
 
+  import { getURL } from '../../stores/url-router.ts'
+  import Clickable from '../clickable.svelte'
   import Icon from '../icon.svelte'
 
   const ICONS = {
@@ -26,18 +28,25 @@
     synchronizedAfterWait: mdiCloudCheckVariant,
     wait: mdiCloudOff
   } satisfies Partial<Record<SyncStatus, string>>
-</script>
 
-{#if $syncStatus !== 'synchronized' && $syncStatus !== 'local'}
-  <div
-    class="navbar-sync"
-    class:is-dangerous={$syncStatus === 'error'}
-    class:is-wait={$syncStatus === 'wait' ||
+  let isWait = $derived(
+    $syncStatus === 'wait' ||
       $syncStatus === 'connectingAfterWait' ||
       $syncStatus === 'disconnected' ||
       $syncStatus === 'connecting' ||
       $syncStatus === 'sending' ||
-      $syncStatus === 'sendingAfterWait'}
+      $syncStatus === 'sendingAfterWait'
+  )
+</script>
+
+{#if $syncStatus !== 'synchronized' && $syncStatus !== 'local'}
+  <Clickable
+    class={{
+      'is-dangerous': $syncStatus === 'error',
+      'is-wait': isWait,
+      'navbar-sync': true
+    }}
+    href={getURL({ params: {}, route: 'profile' })}
   >
     <Icon path={ICONS[$syncStatus]} />
     {#if $syncStatus === 'wait' || $syncStatus === 'disconnected'}
@@ -49,7 +58,7 @@
     {:else}
       {$t[`${$syncStatus}Status`]}
     {/if}
-  </div>
+  </Clickable>
 {/if}
 
 <style>
@@ -58,8 +67,12 @@
       display: flex;
       gap: 0.5rem;
       align-items: center;
-      padding: 0.5rem;
+      padding: 0.25rem 0.5rem;
+      margin: 0.25rem 0 0.25rem 0.25rem;
       font: var(--control-secondary-font);
+      color: var(--text-color);
+      text-decoration: none;
+      border-radius: var(--base-radius);
 
       &.is-wait {
         color: var(--secondary-text-color);
@@ -67,6 +80,21 @@
 
       &.is-dangerous {
         color: var(--dangerous-text-color);
+      }
+
+      &:hover,
+      &:focus-visible {
+        color: var(--text-color);
+        background: --tune-background(--secondary, --secondary-hover);
+      }
+
+      &:active {
+        /* 1px gap on any scale */
+        /* stylelint-disable unit-disallowed-list */
+        padding-top: calc(0.25rem + 1px);
+        padding-bottom: calc(0.25rem - 1px);
+        box-shadow: var(--pressed-shadow);
+        /* stylelint-enable unit-disallowed-list */
       }
 
       @media (width <= 64rem) {
