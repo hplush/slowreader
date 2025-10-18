ea08b485cfa8790d58b1877bf627bf94cb157dc8
Andrey Sitnik <andrey@sitnik.ru>
2025-10-18 19:27:37 +0000

    Fix submenu animation on mobile

--- web/ui/navbar/index.svelte
+++ web/ui/navbar/index.svelte
@@ -211,7 +211,7 @@
         justify-content: space-between;
         width: var(--thin-content-width);
         max-width: 100%;
-        padding: 0.25rem 0;
+        padding: 0.5rem 0;
       }
     }
 
@@ -294,7 +294,7 @@
       flex-shrink: 1;
       flex-direction: column;
       gap: 0.125rem;
-      padding: 0.125rem 0.25rem 0;
+      padding: 0.125rem 0.25rem;
       margin-inline-end: -0.5rem;
       overflow-y: auto;
 
@@ -304,11 +304,16 @@
         max-height: 0;
         margin-inline-end: 0;
         overflow: hidden;
-        transition: max-height 300ms cubic-bezier(0.11, 0, 0.5, 0);
+        transition:
+          max-height 300ms cubic-bezier(0.11, 0, 0.5, 0),
+          padding-block 300ms cubic-bezier(0.11, 0, 0.5, 0);
+
+        &:not(.is-opened) {
+          padding-block: 0;
+        }
 
         &.is-opened {
           max-height: calc(100vh - var(--control-height) - 0.5rem);
-          padding-bottom: 0.125rem;
           overflow: auto;
           transition: max-height 500ms cubic-bezier(0.5, 1, 0.89, 1);
         }
