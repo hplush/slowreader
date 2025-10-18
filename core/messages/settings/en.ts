3e1e51199c80e260b1f264f01d1ea3e9f6b31f5d
Andrey Sitnik <andrey@sitnik.ru>
2025-10-18 21:18:36 +0000

    Add theme control to UI settings page

--- core/messages/settings/en.ts
+++ core/messages/settings/en.ts
@@ -6,15 +6,15 @@ export const settingsMessages = i18n('settings', {
   commonTitle: 'Settings',
   deleteProfile: 'Delete all data',
   deleteProfileConfirm: 'Data deletion cannot be undone. Are you sure?',
-  interface: 'User Interface',
-  preload: 'Feed Loading',
+  interfaceTitle: 'UI',
+  preload: 'Feed Loading Settings',
   preloadAlways: 'Always pre-load media',
   preloadFree: 'Pre-load by Wi-Fi only',
   preloadImages: 'Preload posts images',
   preloadNever: 'Never pre-load media',
   profile: 'Profile',
-  theme: 'Application theme',
-  themeDark: 'Dark theme',
-  themeLight: 'Light theme',
-  themeSystem: 'System theme'
+  theme: 'Theme',
+  themeDark: 'Dark',
+  themeLight: 'Light',
+  themeSystem: 'System'
 })
--- web/pages/interface.svelte
+++ web/pages/interface.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
-  import { settingsMessages as t } from '@slowreader/core'
+  import { mdiThemeLightDark, mdiWeatherNight, mdiWeatherSunny } from '@mdi/js'
+  import { settingsMessages as t, theme } from '@slowreader/core'
 
+  import Radio from '../ui/radio.svelte'
+  import Stack from '../ui/stack.svelte'
   import ThinPage from '../ui/thin-page.svelte'
   import Title from '../ui/title.svelte'
 </script>
 
-<ThinPage title={$t.interface}>
-  <Title>{$t.interface}</Title>
+<ThinPage title={[$t.interfaceTitle, $t.commonTitle]}>
+  <Stack>
+    <Title>{$t.applicationInterface}</Title>
+    <Radio
+      label={$t.theme}
+      store={theme}
+      values={[
+        ['light', $t.themeLight, mdiWeatherSunny],
+        ['system', $t.themeSystem, mdiThemeLightDark],
+        ['dark', $t.themeDark, mdiWeatherNight]
+      ]}
+    />
+  </Stack>
 </ThinPage>
