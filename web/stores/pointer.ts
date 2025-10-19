fa8b49c3da07908470b1b1bdfc66b86eac7f6068
Andrey Sitnik <andrey@sitnik.ru>
2025-10-19 23:37:17 +0000

    Use compressed media queries in JS

--- web/stores/pointer.ts
+++ web/stores/pointer.ts
@@ -4,7 +4,7 @@ import { computed } from 'nanostores'
 
 export const usePointer = persistentBoolean('slowreader:pointer', true)
 
-const $touchOnly = fromMediaQuery('(any-hover: none) and (any-pointer: coarse)')
+const $touchOnly = fromMediaQuery('(any-hover:none) and (any-pointer:coarse)')
 
 export const detectPointer = computed(
   [$touchOnly, usePointer],
