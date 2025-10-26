6102485f60db71a6b605fb74beb981bd08d3d0e7
Andrey Sitnik <andrey@sitnik.ru>
2025-10-26 09:12:03 +0000

    Add feed fields to feed popup

--- core/messages/organize/en.ts
+++ core/messages/organize/en.ts
@@ -14,7 +14,7 @@ export const organizeMessages = i18n('organize', {
     'This will permanently delete the feed, including its statistics and posts. Are you sure?',
   deleteFeed: 'Delete feed',
   deleteFilter: 'Delete filter',
-  fast: 'Fast reading (fun but not deep)',
+  fast: 'Fun',
   feedUrl: 'Feed URL',
   filterAction: 'Filter action',
   filterActionDelete: 'Delete post',
@@ -24,8 +24,8 @@ export const organizeMessages = i18n('organize', {
   invalidFilter: 'Invalid filter query',
   moveFilterDown: 'Move down',
   moveFilterUp: 'Move up',
-  name: 'Feed name',
   renameCategory: 'Rename category',
-  slow: 'Slow reading (deep and meaningful)',
+  slow: 'Useful',
+  title: 'Feed name',
   type: 'Feed type'
 })
--- web/popups/feed.svelte
+++ web/popups/feed.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
-  import { mdiPlusCircleOutline, mdiTrashCanOutline } from '@mdi/js'
-  import { type FeedPopup, organizeMessages as t } from '@slowreader/core'
+  import {
+    mdiFireplace,
+    mdiFood,
+    mdiPlusCircleOutline,
+    mdiTrashCanOutline
+  } from '@mdi/js'
+  import {
+    changeFeed,
+    type FeedPopup,
+    organizeMessages as t
+  } from '@slowreader/core'
 
   import Button from '../ui/button.svelte'
+  import Input from '../ui/input.svelte'
   import Output from '../ui/output.svelte'
   import Popup from '../ui/popup.svelte'
+  import Radio from '../ui/radio.svelte'
   import Stack from '../ui/stack.svelte'
 
   let { popup }: { popup: FeedPopup } = $props()
@@ -28,7 +39,27 @@
       </Button>
     {/if}
   {/snippet}
-  <Stack gap="xl">
+  <Stack gap="l">
     <Output label={$t.feedUrl} value={popup.param} />
+    {#if $feed}
+      <Input
+        label={$t.title}
+        onchange={title => {
+          changeFeed($feed.id, { title })
+        }}
+        value={$feed.title}
+      />
+      <Radio
+        label={$t.type}
+        onchange={reading => {
+          changeFeed($feed.id, { reading })
+        }}
+        value={$feed.reading}
+        values={[
+          ['slow', $t.slow, mdiFireplace],
+          ['fast', $t.fast, mdiFood]
+        ]}
+      />
+    {/if}
   </Stack>
 </Popup>
