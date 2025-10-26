e65b07e93ccc8399c32e696c9073a837bd1d1054
Andrey Sitnik <andrey@sitnik.ru>
2025-10-26 15:22:26 +0000

    Add Select field

---
+++ web/stories/ui/select.stories.svelte
@@ -0,0 +1,171 @@
+<script lang="ts" module>
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+
+  import Select from '../../ui/select.svelte'
+  import Section from '../section.svelte'
+
+  let { Story } = defineMeta({
+    component: Select,
+    title: 'UI/Select'
+  })
+</script>
+
+<script lang="ts">
+  type Value = '1' | '2' | '3'
+
+  let value = $state<Value>('2')
+
+  function onchange(v: Value): void {
+    value = v
+  }
+</script>
+
+<Story name="Light" asChild>
+  <Section width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section hover width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section focus width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section active width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'A very very very long option value'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section main width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+</Story>
+
+<Story name="Dark" asChild parameters={{ themes: { themeOverride: 'dark' } }}>
+  <Section width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section hover width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section focus width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section active width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'A very very very long option value'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+  <Section main width={200}>
+    <Select
+      label="Label"
+      {onchange}
+      {value}
+      values={[
+        ['1', 'First'],
+        ['2', 'Second'],
+        ['3', 'Third']
+      ]}
+    />
+  </Section>
+</Story>
---
+++ web/ui/select.svelte
@@ -0,0 +1,104 @@
+<script generics="Value extends string" lang="ts">
+  import { mdiChevronDown } from '@mdi/js'
+
+  import Icon from './icon.svelte'
+  import Label from './label.svelte'
+
+  let {
+    label,
+    onchange,
+    value,
+    values
+  }: {
+    label: string
+    onchange: (value: Value) => void
+    value: Value
+    values: [Value, string][]
+  } = $props()
+
+  let id = $props.id()
+
+  let current = $derived.by(() => {
+    let currentOption = values.find(i => i[0] === value)
+    return currentOption ? currentOption[1] : ' '
+  })
+</script>
+
+<div class="select">
+  <Label {id}>{label}</Label>
+  <div class="select_fake">
+    <div class="select_overflow">
+      <div class="select_text">{current}</div>
+    </div>
+    <Icon path={mdiChevronDown} />
+    <select
+      {id}
+      class="select_select"
+      onchange={e => {
+        onchange(e.currentTarget.value as Value)
+      }}
+    >
+      {#each values as [key, name] (key)}
+        <option selected={value === key} value={key}>{name}</option>
+      {/each}
+    </select>
+  </div>
+</div>
+
+<style>
+  :global {
+    .select {
+      width: stretch;
+    }
+
+    .select_fake {
+      position: relative;
+      box-sizing: border-box;
+      display: flex;
+      flex-shrink: 1;
+      gap: 0.25rem;
+      align-items: center;
+      justify-content: space-between;
+      height: var(--control-height);
+      padding: 0 var(--control-padding);
+      overflow: hidden;
+      font: var(--control-font);
+      background: --tune-background(--field);
+      border-radius: var(--base-radius);
+      box-shadow: var(--field-shadow);
+      corner-shape: squircle;
+
+      &:has(.select_select:focus-visible) {
+        outline: 3px solid var(--focus-color);
+        outline-offset: 2px;
+        transition:
+          outline-width var(--focus-time) var(--pop-easing),
+          outline-offset var(--focus-time) var(--pop-easing);
+      }
+
+      &:has(.select_select:open) {
+        padding-top: var(--min-size);
+        box-shadow: var(--pressed-shadow);
+      }
+    }
+
+    .select_overflow {
+      flex-shrink: 1;
+      overflow: hidden;
+    }
+
+    .select_text {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    .select_select {
+      position: absolute;
+      inset: 0;
+      font: revert;
+      appearance: none;
+      opacity: 0%;
+    }
+  }
+</style>
