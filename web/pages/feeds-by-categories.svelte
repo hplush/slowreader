e7668b3a716b7fcfd2e2813ef0968248734f47a8
Andrey Sitnik <andrey@sitnik.ru>
2025-11-01 21:06:56 +0000

    Add empty feeds state

--- web/pages/feeds-by-categories.svelte
+++ web/pages/feeds-by-categories.svelte
@@ -9,6 +9,7 @@
 
   import Button from '../ui/button.svelte'
   import Feeds from '../ui/feeds.svelte'
+  import NoFeeds from '../ui/no-feeds.svelte'
   import PopupablePage from '../ui/popupable-page.svelte'
   import Stack from '../ui/stack.svelte'
   import Title from '../ui/title.svelte'
@@ -19,50 +20,54 @@
 </script>
 
 <PopupablePage padding title={[$t.byCategoryTitle, $t.feedsTitle]}>
-  <Stack gap="xl" role="menu">
-    {#each $groups as [category, feeds] (category.id)}
-      <Stack>
-        <Stack
-          align="baseline"
-          controls={`${category.id}-feeds`}
-          gap="s"
-          justify="space-between"
-          role="menuitem"
-          row
-        >
-          <Title>{category.title}</Title>
-          {#if category.id !== 'general' && category.id !== 'broken'}
-            <Stack gap="s" row width="auto">
-              <Button
-                icon={mdiRenameOutline}
-                onclick={() => {
-                  let title = prompt($t.categoryName, category.title)
-                  if (title) {
-                    changeCategory(category.id, { title })
-                  }
-                }}
-                size="icon"
-                variant="secondary"
-              >
-                {$t.renameCategory}
-              </Button>
-              <Button
-                icon={mdiTrashCanOutline}
-                onclick={() => {
-                  if (confirm($t.deleteCategoryConform)) {
-                    deleteCategory(category.id)
-                  }
-                }}
-                size="icon"
-                variant="secondary-dangerous"
-              >
-                {$t.deleteCategory}
-              </Button>
-            </Stack>
-          {/if}
+  {#if $groups.length === 0}
+    <NoFeeds />
+  {:else}
+    <Stack gap="xl" role="menu">
+      {#each $groups as [category, feeds] (category.id)}
+        <Stack>
+          <Stack
+            align="baseline"
+            controls={`${category.id}-feeds`}
+            gap="s"
+            justify="space-between"
+            role="menuitem"
+            row
+          >
+            <Title>{category.title}</Title>
+            {#if category.id !== 'general' && category.id !== 'broken'}
+              <Stack gap="s" row width="auto">
+                <Button
+                  icon={mdiRenameOutline}
+                  onclick={() => {
+                    let title = prompt($t.categoryName, category.title)
+                    if (title) {
+                      changeCategory(category.id, { title })
+                    }
+                  }}
+                  size="icon"
+                  variant="secondary"
+                >
+                  {$t.renameCategory}
+                </Button>
+                <Button
+                  icon={mdiTrashCanOutline}
+                  onclick={() => {
+                    if (confirm($t.deleteCategoryConform)) {
+                      deleteCategory(category.id)
+                    }
+                  }}
+                  size="icon"
+                  variant="secondary-dangerous"
+                >
+                  {$t.deleteCategory}
+                </Button>
+              </Stack>
+            {/if}
+          </Stack>
+          <Feeds id={`${category.id}-feeds`} current={$opened} list={feeds} />
         </Stack>
-        <Feeds id={`${category.id}-feeds`} current={$opened} list={feeds} />
-      </Stack>
-    {/each}
-  </Stack>
+      {/each}
+    </Stack>
+  {/if}
 </PopupablePage>
---
+++ web/ui/no-feeds.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { mdiPackageVariant } from '@mdi/js'
+  import { organizeMessages as t } from '@slowreader/core'
+
+  import { getURL } from '../stores/url-router.ts'
+  import Button from './button.svelte'
+  import Placeholder from './placeholder.svelte'
+  import Stack from './stack.svelte'
+</script>
+
+<Placeholder icon={mdiPackageVariant}>
+  <Stack>
+    {$t.emptyText}
+    <Stack gap="s" row>
+      <Button href={getURL('add')}>{$t.emptyAdd}</Button>
+      <Button href={getURL('import')}>{$t.emptyImport}</Button>
+    </Stack>
+  </Stack>
+</Placeholder>
