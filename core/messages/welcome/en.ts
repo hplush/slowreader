7d38dd4485d6838c4cb5e7275ced64a31acd5601
Andrey Sitnik <andrey@sitnik.ru>
2025-11-15 09:15:46 +0000

    Add slow/fast descriptions

--- core/messages/welcome/en.ts
+++ core/messages/welcome/en.ts
@@ -1,6 +1,8 @@
 import { i18n } from '../../i18n.ts'
 
 export const welcomeMessages = i18n('welcome', {
-  fastDesc: 'Fast',
-  slowDesc: 'Slow'
+  fastDesc:
+    'Place here deep fun content which you can miss.\n\nFor example: short videos, jokes, news with low signal-to-noise ratio, guilty pleasure.',
+  slowDesc:
+    'Place here deep, long-form content meant to be read slowly and mindfully.\n\nThe sources that focus on **knowledge rather than emotion** and prioritize **quality over quantity**.\n\nFor example: expert’s blogs, newsletters, book summaries, authors who write detailed threads.'
 })
