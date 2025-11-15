0696b67b25cd849f665f73c3cbad713da39246fd
Andrey Sitnik <andrey@sitnik.ru>
2025-11-15 09:49:10 +0000

    Add empty pages

--- core/messages/welcome/en.ts
+++ core/messages/welcome/en.ts
@@ -3,6 +3,10 @@ import { i18n } from '../../i18n.ts'
 export const welcomeMessages = i18n('welcome', {
   fastDesc:
     'Place here deep fun content which you can miss.\n\nFor example: short videos, jokes, news with low signal-to-noise ratio, guilty pleasure.',
+  fastEmpty: 'You read all Fun posts',
+  openSlow: 'Read Useful posts',
+  refresh: 'Download new posts',
   slowDesc:
-    'Place here deep, long-form content meant to be read slowly and mindfully.\n\nThe sources that focus on **knowledge rather than emotion** and prioritize **quality over quantity**.\n\nFor example: expert’s blogs, newsletters, book summaries, authors who write detailed threads.'
+    'Place here deep, long-form content meant to be read slowly and mindfully.\n\nThe sources that focus on **knowledge rather than emotion** and prioritize **quality over quantity**.\n\nFor example: expert’s blogs, newsletters, book summaries, authors who write detailed threads.',
+  slowEmpty: 'You read all Useful posts'
 })
--- web/pages/feeds/empty.svelte
+++ web/pages/feeds/empty.svelte
@@ -1,7 +1,54 @@
 <script lang="ts">
-  import type { EmptyReader } from '@slowreader/core'
+  import {
+    mdiFireplace,
+    mdiFireplaceOff,
+    mdiReload,
+    mdiWhiteBalanceSunny
+  } from '@mdi/js'
+  import {
+    type EmptyReader,
+    feedsMessages,
+    isRefreshing,
+    refreshPosts,
+    refreshProgress,
+    welcomeMessages as t
+  } from '@slowreader/core'
+
+  import { getURL } from '../../stores/url-router.ts'
+  import Button from '../../ui/button.svelte'
+  import PageIcon from '../../ui/page-icon.svelte'
+  import PopupablePage from '../../ui/popupable-page.svelte'
+  import Stack from '../../ui/stack.svelte'
 
   let { reader }: { reader: EmptyReader } = $props()
 </script>
 
-Empty {reader.reading}
+{#if reader.reading === 'fast'}
+  <PopupablePage title={$feedsMessages.fastTitle}>
+    <PageIcon path={mdiWhiteBalanceSunny}>
+      <Stack align="center" gap="l">
+        {$t.fastEmpty}
+        <div class="is-comfort-mode">
+          <Button href={getURL('slow')} icon={mdiFireplace}
+            >{$t.openSlow}</Button
+          >
+        </div>
+      </Stack>
+    </PageIcon>
+  </PopupablePage>
+{:else}
+  <PopupablePage title={$feedsMessages.slowTitle}>
+    <PageIcon path={mdiFireplaceOff}>
+      <Stack align="center" gap="l">
+        {$t.slowEmpty}
+        <div class="is-comfort-mode">
+          <Button
+            icon={mdiReload}
+            loader={$isRefreshing ? $refreshProgress : undefined}
+            onclick={refreshPosts}>{$t.refresh}</Button
+          >
+        </div>
+      </Stack>
+    </PageIcon>
+  </PopupablePage>
+{/if}
