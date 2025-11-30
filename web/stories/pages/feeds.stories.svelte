fce1c0f36a04b5e0cb8c8ea570a38029fae9e6c4
Andrey Sitnik <andrey@sitnik.ru>
2025-11-30 23:59:36 +0000

    Add feeds list stories

--- web/stories/pages/feeds.stories.svelte
+++ web/stories/pages/feeds.stories.svelte
@@ -1,9 +1,48 @@
 <script context="module" lang="ts">
-  import { needWelcome, pages } from '@slowreader/core'
+  import {
+    changePost,
+    needWelcome,
+    pages,
+    type PostValue
+  } from '@slowreader/core'
   import { defineMeta } from '@storybook/addon-svelte-csf'
 
   import FeedsPage from '../../pages/feeds/index.svelte'
   import Scene from '../scene.svelte'
+
+  const POSTS = [
+    {
+      feedId: 'feed',
+      reading: 'slow',
+      title: 'My next chapter with Mastodon'
+    },
+    { feedId: 'feed', reading: 'slow' },
+    {
+      feedId: 'feed',
+      reading: 'slow',
+      title: 'The Future is Ours to Build - Together'
+    },
+    { feedId: 'feed', full: '', intro: '', reading: 'slow', title: '' },
+    {
+      feedId: 'feed',
+      reading: 'slow',
+      title:
+        'Linux Patches Improve Intel Nested VM Memory Performance Up To ~2353x In Synthetic Test'
+    },
+    {
+      feedId: 'feed',
+      reading: 'slow',
+      title: '6000000000000000000000000000000000000000000000000000000000000'
+    }
+  ] satisfies Partial<PostValue>[]
+
+  function multiply<Value>(array: Value[], count: number): Value[] {
+    let result: Value[] = []
+    for (let i = 0; i < count; i++) {
+      result.push(...array)
+    }
+    return result
+  }
 
   let { Story } = defineMeta({
     component: FeedsPage,
@@ -58,6 +97,49 @@
     }}
     posts={[{ feedId: 'feed', reading: 'slow' }]}
     route={{ params: { feed: 'feed' }, route: 'slow' }}
+  >
+    <FeedsPage page={pages.slow()} />
+  </Scene>
+</Story>
+
+<Story name="List" asChild parameters={{ layout: 'fullscreen' }}>
+  <Scene
+    feeds={[{ id: 'feed', reading: 'slow' }]}
+    oninit={() => {
+      setTimeout(() => {
+        changePost('post-2', { read: true })
+        changePost('post-3', { read: true })
+      }, 1)
+    }}
+    posts={POSTS}
+    route={{ params: { feed: 'feed' }, route: 'slow' }}
+  >
+    <FeedsPage page={pages.slow()} />
+  </Scene>
+</Story>
+
+<Story name="List Openned" asChild parameters={{ layout: 'fullscreen' }}>
+  <Scene
+    feeds={[{ id: 'feed', reading: 'slow' }]}
+    posts={POSTS}
+    route={{
+      hash: 'post=read:post-1',
+      params: { feed: 'feed' },
+      route: 'slow'
+    }}
+  >
+    <FeedsPage page={pages.slow()} />
+  </Scene>
+</Story>
+
+<Story name="List Pagination" asChild parameters={{ layout: 'fullscreen' }}>
+  <Scene
+    feeds={[{ id: 'feed', reading: 'slow' }]}
+    posts={multiply(POSTS, 50)}
+    route={{
+      params: { feed: 'feed' },
+      route: 'slow'
+    }}
   >
     <FeedsPage page={pages.slow()} />
   </Scene>
