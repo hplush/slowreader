bff5593259af6d6d8afff856e44a46b318233857
Andrey Sitnik <andrey@sitnik.ru>
2026-03-14 23:31:03 +0000

    Clarify hack in comment

--- server/modules/added.ts
+++ server/modules/added.ts
@@ -8,6 +8,7 @@ const NEXT_QUERY = sql.raw(`SELECT nextval('"${actionsAdded.seqName}"')`)
 export default (server: BaseServer): void => {
   server.log.store.getLastAdded = async () => {
     let result = await db.execute(NEXT_QUERY)
+    // Different DB drivers return different format
     let rows = Array.isArray(result)
       ? (result as { nextval: number }[]) // pglite
       : (result as { rows: { nextval: string }[] }).rows // postgres.js
