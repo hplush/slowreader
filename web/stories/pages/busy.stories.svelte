16e697cbefa0d8a1a3a95b32b4f7a68dccff6246
Andrey Sitnik <andrey@sitnik.es>
2026-08-02 21:15:59 +0000

    Add ability to add label for app is busy loader

--- web/stories/pages/busy.stories.svelte
+++ web/stories/pages/busy.stories.svelte
@@ -1,7 +1,9 @@
 <script context="module" lang="ts">
+  import { busy } from '@slowreader/core'
   import { defineMeta } from '@storybook/addon-svelte-csf'
 
   import BusyPage from '../../pages/busy.svelte'
+  import Scene from '../scene.svelte'
 
   let { Story } = defineMeta({
     component: BusyPage,
@@ -9,4 +11,50 @@
   })
 </script>
 
-<Story name="Base" parameters={{ layout: 'fullscreen' }} />
+<Story name="Base" asChild parameters={{ layout: 'fullscreen' }}>
+  <Scene
+    oninit={() => {
+      busy.set({})
+    }}
+  >
+    <BusyPage />
+  </Scene>
+</Story>
+
+<Story name="Label" asChild parameters={{ layout: 'fullscreen' }}>
+  <Scene
+    oninit={() => {
+      busy.set({ label: 'Creating database' })
+    }}
+  >
+    <BusyPage />
+  </Scene>
+</Story>
+
+<Story name="Progress" asChild parameters={{ layout: 'fullscreen' }}>
+  <Scene
+    oninit={() => {
+      busy.set({
+        label: 'Creating benchmark data',
+        progress: 0.72
+      })
+    }}
+  >
+    <BusyPage />
+  </Scene>
+</Story>
+
+<Story name="Long Label" asChild parameters={{ layout: 'fullscreen' }}>
+  <Scene
+    oninit={() => {
+      busy.set({
+        label:
+          'Creating benchmark data with a very long description, ' +
+          'which should be wrapped to a few lines on any screen',
+        progress: 0.72
+      })
+    }}
+  >
+    <BusyPage />
+  </Scene>
+</Story>
