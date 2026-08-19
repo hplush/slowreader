fe804768a71192a417d18e0ddd56543f876ef6b5
Andrey Sitnik <andrey@sitnik.es>
2026-08-19 08:34:11 +0000

    Fix loader-tests with new JSDom

--- loader-tests/dom-api.ts
+++ loader-tests/dom-api.ts
@@ -1,6 +1,7 @@
 import { JSDOM } from 'jsdom'
 
-let window = new JSDOM().window
+// Without URL, JSDOM has opaque origin and throws on `window.localStorage`
+let window = new JSDOM('', { url: 'https://slowreader.app/' }).window
 // @ts-expect-error JSDOM types are incomplete
 global.window = window
 global.DOMParser = window.DOMParser
