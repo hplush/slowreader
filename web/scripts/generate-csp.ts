a981d7e830a35a9fc6029433155317d3c5a83cfb
Andrey Sitnik <andrey@sitnik.es>
2026-08-30 09:16:07 +0000

    Fix CSP for logo in navbar

--- web/scripts/generate-csp.ts
+++ web/scripts/generate-csp.ts
@@ -9,22 +9,26 @@ import { join } from 'node:path'
 const NGINX = join(import.meta.dirname, '../nginx.conf')
 const LOADER = join(import.meta.dirname, '../dist/index.html')
 const ERROR = join(import.meta.dirname, '../dist/404.html')
+const ICON = join(import.meta.dirname, '../dist/icon.svg')
 
 function hash(body: string): string {
   return `'sha256-${createHash('sha256').update(body).digest('base64')}'`
 }
 
-let [loader, error, nginx] = await Promise.all([
+let [loader, error, icon, nginx] = await Promise.all([
   readFile(LOADER, 'utf8'),
   readFile(ERROR, 'utf8'),
+  readFile(ICON, 'utf8'),
   readFile(NGINX, 'utf8')
 ])
 let loaderStyles = loader.match(/<style>[\s\S]*?<\/style>/gi)!
 let errorCSS = error.match(/<style>[\s\S]*?<\/style>/gi)!
+let iconCSS = icon.match(/<style>[\s\S]*?<\/style>/gi)!
 let loaderJS = loader.match(/<script>([\s\S]*?)<\/script>/i)![1]!
 
 let hashesCSS = loaderStyles
   .concat(errorCSS)
+  .concat(iconCSS)
   .map(i => hash(i.slice(7, -8)))
   .join(' ')
 
