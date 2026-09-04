4786708c86bc74aa6e36db366ff9f16033ff3d7d
Andrey Sitnik <andrey@sitnik.es>
2026-09-04 17:52:55 +0000

    Clean up extension

--- extension/manifest.config.ts
+++ extension/manifest.config.ts
@@ -3,19 +3,19 @@ import { defineManifest } from '@crxjs/vite-plugin'
 const URL =
   process.env.NODE_ENV === 'production'
     ? 'https://*.slowreader.app/*'
-    : 'http://localhost:5173/*'
+    : 'http://localhost:2553/*'
 
 export default defineManifest(() => ({
   background: {
     service_worker: 'background.ts',
     type: 'module'
   },
-  description: 'Fetch data from websites for dev.slowreader.app',
+  description: 'Companion extension for Slow Reader to speed-up feeds update',
   externally_connectable: {
     matches: [URL]
   },
   host_permissions: [URL],
   manifest_version: 3,
-  name: 'Slowreader Extension',
+  name: 'Slow Reader Extension',
   version: '0.0.1'
 }))
