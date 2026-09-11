804ba57ee83121700ccaa3d13d257db52bccbbe7
Andrey Sitnik <andrey@sitnik.es>
2026-09-11 14:56:28 +0000

    Fix landing on mobile

--- landings/postcss.config.ts
+++ landings/postcss.config.ts
@@ -1,7 +1,15 @@
+import { join } from 'node:path'
 import shadows from 'postcss-smooth-shadow'
 
+import customMedia from '../web/postcss/custom-media.ts'
 import squircleCorner from '../web/postcss/squircle-corner.ts'
 
 export default {
-  plugins: [squircleCorner, shadows()]
+  plugins: [
+    customMedia({
+      file: join(import.meta.dirname, '..', 'web', 'main', 'sizes.css')
+    }),
+    squircleCorner,
+    shadows()
+  ]
 }
