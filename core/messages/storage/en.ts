c7d70a2142ba5a7ea93783d76222c6d349c62d4d
Andrey Sitnik <andrey@sitnik.es>
2026-09-11 23:40:22 +0000

    Better translation

--- core/messages/storage/en.ts
+++ core/messages/storage/en.ts
@@ -7,7 +7,7 @@ export const storageMessages = i18n('storage', {
   deleteData: 'Delete all local data',
   deleteWarning:
     'This action cannot be undone. Are you sure you want to delete your data?',
-  exportFile: 'Debug DB',
+  exportFile: 'Dump DB',
   exporting: 'Exporting the database',
   pageTitle: 'Storage',
   rebuild: 'Rebuild database from the cloud',
