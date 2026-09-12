41c99d2d051d5b8befeef6924bb328e1375f3ef3
Andrey Sitnik <andrey@sitnik.es>
2026-09-12 09:14:53 +0000

    Fix line breaks

--- core/messages/fatal/en.ts
+++ core/messages/fatal/en.ts
@@ -26,7 +26,7 @@ export const fatalMessages = i18n('fatal', {
   secondTabDescription:
     'Safari and some other browsers can save the data only from a single ' +
     'tab. Close the other tab, and this page will open the app by itself.',
-  secondTabText: 'Slow Reader is open in another tab',
+  secondTabText: 'Slow Reader is open in another tab',
   secondTabTitle: 'No multi-tab',
   updateButton: 'Update client now'
 })
